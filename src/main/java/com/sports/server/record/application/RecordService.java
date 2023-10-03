f16861d186ba2dff6013d303168d5c95c9e1c011
Jin409 <sualng123@gmail.com>
2023-10-03 23:37:17 +0900

    [DEL] 양방향 매핑에 따라 사용하지 않는 record 전체 조회 기능 삭제 #5

--- src/main/java/com/sports/server/record/application/RecordService.java
+++ src/main/java/com/sports/server/record/application/RecordService.java
@@ -1,13 +1,9 @@
 package com.sports.server.record.application;
 
-import com.sports.server.game.domain.Game;
 import com.sports.server.record.domain.RecordRepository;
-import com.sports.server.record.dto.response.RecordResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -15,8 +11,4 @@ import java.util.List;
 public class RecordService {
 
     private final RecordRepository recordRepository;
-
-    public List<RecordResponseDto> findAllRecordsWithGame(final Game game) {
-        return recordRepository.findAllByGame(game).stream().map(RecordResponseDto::new).toList();
-    }
 }
--- src/main/java/com/sports/server/record/domain/RecordRepository.java
+++ src/main/java/com/sports/server/record/domain/RecordRepository.java
@@ -1,12 +1,7 @@
 package com.sports.server.record.domain;
 
-import com.sports.server.game.domain.Game;
 import org.springframework.data.repository.Repository;
 
-import java.util.List;
-
 public interface RecordRepository extends Repository<Record, Long> {
-
-    List<Record> findAllByGame(final Game game);
 
 }
