b6a28ec5049174c6cfb3018edd66b7cb050b65b0
Jin409 <sualng123@gmail.com>
2023-10-03 11:01:37 +0900

    [CHORE] 초기 세팅

---
+++ src/test/java/com/sports/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sports.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
