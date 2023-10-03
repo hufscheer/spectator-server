21b07b2526919429e996d75c73c4eb12dbe26f1c
Jin409 <sualng123@gmail.com>
2023-10-03 15:37:54 +0900

    [FEAT] 팀 전체 조회 기능 구현 #3

--- src/main/java/com/sports/server/team/TeamRepository.java
+++ src/main/java/com/sports/server/team/TeamRepository.java
@@ -3,10 +3,12 @@ package com.sports.server.team;
 import com.sports.server.team.domain.Team;
 import org.springframework.data.repository.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TeamRepository extends Repository<Team, Long> {
 
     Optional<Team> findById(final Long id);
 
+    List<Team> findAll();
 }
