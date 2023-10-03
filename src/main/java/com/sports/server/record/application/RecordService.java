87cbf1bc4a443ad91c27d952fd5b2bc5ee1d2d31
Jin409 <sualng123@gmail.com>
2023-10-03 16:03:07 +0900

    [ADD] GameService 에 @Transactional 어노테이션 추가 #5

--- src/main/java/com/sports/server/record/application/RecordService.java
+++ src/main/java/com/sports/server/record/application/RecordService.java
@@ -5,11 +5,13 @@ import com.sports.server.record.domain.RecordRepository;
 import com.sports.server.record.dto.response.RecordResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class RecordService {
 
     private final RecordRepository recordRepository;
