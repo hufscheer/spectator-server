7a6720c4d3debaa5d6ce4282530ba21b61fa3913
Jin409 <sualng123@gmail.com>
2023-10-03 23:36:26 +0900

    [FEAT] 경기 상세 조회 기능 구현 #5

--- src/main/java/com/sports/server/game/domain/GameRepository.java
+++ src/main/java/com/sports/server/game/domain/GameRepository.java
@@ -2,6 +2,10 @@ package com.sports.server.game.domain;
 
 import org.springframework.data.repository.Repository;
 
+import java.util.Optional;
+
 public interface GameRepository extends Repository<Game, Long> {
     void save(final Game game);
+
+    Optional<Game> findById(final Long id);
 }
--- src/main/java/com/sports/server/game/presentation/GameController.java
+++ src/main/java/com/sports/server/game/presentation/GameController.java
@@ -2,13 +2,11 @@ package com.sports.server.game.presentation;
 
 import com.sports.server.game.application.GameService;
 import com.sports.server.game.dto.request.GameRegisterRequestDto;
+import com.sports.server.game.dto.response.GameResponseDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,5 +20,10 @@ public class GameController {
         gameService.register(requestDto);
         // TODO: game 전체 조회 기능 구현 이후에 ResponseEntity.created 로 변경하기
         return ResponseEntity.ok("");
+    }
+
+    @GetMapping("/{gameId}")
+    public ResponseEntity<GameResponseDto> getOneGame(@PathVariable Long gameId) {
+        return ResponseEntity.ok(gameService.getOneGame(gameId));
     }
 }
