27eaa82f7fca9dd33bea93f9301741c81171be36
Jin409 <sualng123@gmail.com>
2023-10-03 13:02:00 +0900

    [ADD] Record 엔티티 추가 #1

---
+++ src/main/java/com/sports/server/record/domain/Record.java
@@ -0,0 +1,29 @@
+package com.sports.server.record.domain;
+
+import com.sports.server.game.domain.Game;
+import com.sports.server.team.domain.Team;
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+public class Record {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "game_id")
+    private Game game;
+
+    @ManyToOne
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    @Column(name = "player_name", nullable = false)
+    private String playerName;
+
+    @Column(name = "score", nullable = false)
+    private int score;
+}
