982324978f4cab5d20dfc66318d05877f7970526
Jin409 <sualng123@gmail.com>
2023-10-04 22:44:46 +0900

    [ADD] 엔티티 수정에 따른 컬럼 추가 #8

--- src/main/java/com/sports/server/record/dto/response/RecordResponseDto.java
+++ src/main/java/com/sports/server/record/dto/response/RecordResponseDto.java
@@ -3,21 +3,32 @@ package com.sports.server.record.dto.response;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.sports.server.record.domain.Record;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
+@Getter
 @NoArgsConstructor
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class RecordResponseDto {
 
     private Long id;
+
     private Long teamId;
+
     private String playerName;
+
     private int score;
+
+    private LocalDateTime scoredAt;
+
 
     public RecordResponseDto(final Record record) {
         this.id = record.getId();
         this.teamId = record.getTeam().getId();
         this.playerName = record.getPlayerName();
         this.score = 1; // TODO: 추후 경기 종목 테이블 추가에 따른 리팩토링
+        this.scoredAt = record.getScoredAt();
     }
 }
