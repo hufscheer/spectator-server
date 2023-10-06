6851866f9fa62dc1ffa6839753ba77bbb233199e
Jin409 <sualng123@gmail.com>
2023-10-06 15:17:52 +0900

    [REFACTOR] 경기 기록을 시간 순으로 정렬하도록 수정 #13

--- src/main/java/com/sports/server/game/dto/response/GameDetailResponseDto.java
+++ src/main/java/com/sports/server/game/dto/response/GameDetailResponseDto.java
@@ -1,10 +1,12 @@
 package com.sports.server.game.dto.response;
 
 import com.sports.server.game.domain.Game;
+import com.sports.server.record.domain.Record;
 import com.sports.server.record.dto.response.RecordResponseDto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.Comparator;
 import java.util.List;
 
 @Getter
@@ -15,6 +17,11 @@ public class GameDetailResponseDto extends GameResponseDto {
 
     public GameDetailResponseDto(Game game) {
         super(game);
-        this.records = game.getRecords().stream().map(RecordResponseDto::new).toList();
+        this.records = game.getRecords()
+                .stream()
+                .sorted(Comparator.comparing(Record::getScoredAt))
+                .map(RecordResponseDto::new)
+                .toList();
+
     }
 }
