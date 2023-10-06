252f3a2872c54eff6f1a1301a18354d81c01d736
Jin409 <sualng123@gmail.com>
2023-10-07 01:00:30 +0900

    [ADD] Comment 엔티티 추가 #21

---
+++ src/main/java/com/sports/server/comment/Comment.java
@@ -0,0 +1,24 @@
+package com.sports.server.comment;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Comment {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private LocalDateTime createdAt;
+
+    private String content;
+
+}
