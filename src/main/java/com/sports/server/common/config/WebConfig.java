c6e6de54f7cb54c06a57540d6b8e589cc72ae1bd
Jin409 <sualng123@gmail.com>
2023-10-09 04:45:45 +0900

    [ADD] 프론트 배포 서버 cors 세팅에 추가

--- src/main/java/com/sports/server/common/config/WebConfig.java
+++ src/main/java/com/sports/server/common/config/WebConfig.java
@@ -10,12 +10,14 @@ public class WebConfig implements WebMvcConfigurer {
     private static final String ALLOW_ALL_PATH = "/**";
     private static final String ALLOWED_METHODS = "*";
     private static final String FRONTEND_LOCALHOST = "http://localhost:3000";
+    private static final String FRONTEND_SERVER = "https://hufstreaming.site";
+    private static final String FRONTEND_SERVER_TMP = "https://hufstreaming-client.vercel.app";
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping(ALLOW_ALL_PATH)
                 .allowedMethods(ALLOWED_METHODS)
-                .allowedOrigins(FRONTEND_LOCALHOST);
+                .allowedOrigins(FRONTEND_LOCALHOST, FRONTEND_SERVER, FRONTEND_SERVER_TMP);
     }
 
 }
