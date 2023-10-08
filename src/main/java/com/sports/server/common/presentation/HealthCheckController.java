3bd69b560061e39d7a58e16ab0e0585c2691d114
Jin409 <sualng123@gmail.com>
2023-10-08 14:46:47 +0900

    [ADD] 헬스 체크를 위한 컨트롤러 추가

---
+++ src/main/java/com/sports/server/common/presentation/HealthCheckController.java
@@ -0,0 +1,16 @@
+package com.sports.server.common.presentation;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HealthCheckController {
+
+    @GetMapping
+    public ResponseEntity<Void> healthCheck() {
+        return ResponseEntity.ok(null);
+    }
+}
