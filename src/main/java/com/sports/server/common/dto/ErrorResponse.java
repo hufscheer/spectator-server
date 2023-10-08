582c09d1b5644415b73b73bb74135e7bc417f917
Jin409 <sualng123@gmail.com>
2023-10-08 22:27:47 +0900

    [ADD] FieldError 를 추가한다. #7

--- src/main/java/com/sports/server/common/dto/ErrorResponse.java
+++ src/main/java/com/sports/server/common/dto/ErrorResponse.java
@@ -1,6 +1,11 @@
 package com.sports.server.common.dto;
 
 import lombok.*;
+import org.springframework.validation.BindingResult;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @NoArgsConstructor
@@ -9,11 +14,48 @@ import lombok.*;
 public class ErrorResponse {
 
     private String message;
+    private List<FieldError> fieldErrors;
 
-    public static ErrorResponse createWithMessage(final String message) {
+    public static ErrorResponse of(final String message) {
         return ErrorResponse.builder()
                 .message(message)
                 .build();
+    }
+
+    public static ErrorResponse of(final BindingResult bindingResult) {
+        return ErrorResponse.builder()
+                .fieldErrors(FieldError.of(bindingResult))
+                .build();
+    }
+
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class FieldError {
+        private String field;
+        private String value;
+        private String reason;
+
+        private FieldError(final String field, final String value, final String reason) {
+            this.field = field;
+            this.value = value;
+            this.reason = reason;
+        }
+
+        public static List<FieldError> of(final String field, final String value, final String reason) {
+            List<FieldError> fieldErrors = new ArrayList<>();
+            fieldErrors.add(new FieldError(field, value, reason));
+            return fieldErrors;
+        }
+
+        private static List<FieldError> of(final BindingResult bindingResult) {
+            final List<org.springframework.validation.FieldError> fieldErrors = bindingResult.getFieldErrors();
+            return fieldErrors.stream()
+                    .map(error -> new FieldError(
+                            error.getField(),
+                            error.getRejectedValue() == null ? "" : error.getRejectedValue().toString(),
+                            error.getDefaultMessage()))
+                    .collect(Collectors.toList());
+        }
     }
 
 }
