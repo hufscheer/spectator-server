53bcc6830dfa62257581bb8785f4289cd3dd0745
Jin409 <sualng123@gmail.com>
2023-11-01 16:28:36 +0900

    [ADD] Game 엔티티에 GameTeam 과의 양방향 매핑 추가 #34

--- src/main/java/com/sports/server/game/domain/Game.java
+++ src/main/java/com/sports/server/game/domain/Game.java
@@ -54,4 +54,7 @@ public class Game {
     @OneToMany(mappedBy = "game")
     private List<Record> records = new ArrayList<>();
 
+    @OneToMany(mappedBy = "game")
+    private List<GameTeam> teams = new ArrayList<>();
+
 }
