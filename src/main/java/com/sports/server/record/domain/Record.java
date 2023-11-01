0c169570419b02607be1a5f822e14408600ee309
Jin409 <sualng123@gmail.com>
2023-11-02 01:18:13 +0900

    [REFACTOR] Record 엔티티 기본 생성자의 접근 제어자를 protected 로 수정 #34

--- src/main/java/com/sports/server/record/domain/Record.java
+++ src/main/java/com/sports/server/record/domain/Record.java
@@ -11,12 +11,13 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import java.time.LocalDateTime;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Record {
 
     @Id
