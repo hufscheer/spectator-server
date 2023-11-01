cce374a604b164b4a24c89a1d348d93764de8abf
Jin409 <sualng123@gmail.com>
2023-11-01 13:19:35 +0900

    [ADD] Report 엔티티 추가 #34

---
+++ src/main/java/com/sports/server/comment/domain/Report.java
@@ -0,0 +1,31 @@
+package com.sports.server.comment.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Report {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "comment_id")
+    private Comment comment;
+
+    @Column(name = "reported_at", nullable = false)
+    private LocalDateTime reportedAt;
+
+    @Column(name = "is_valid", nullable = false)
+    private boolean isValid;
+}
