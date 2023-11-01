e8220393127d25e243c2010d18c202c2d910639b
Jin409 <sualng123@gmail.com>
2023-11-01 16:29:41 +0900

    [ADD] 팀 id 관련 유효성 검사 메시지 추가 #34

--- src/main/java/com/sports/server/common/ValidatorMessages.java
+++ src/main/java/com/sports/server/common/ValidatorMessages.java
@@ -2,6 +2,6 @@ package com.sports.server.common;
 
 public class ValidatorMessages {
 
-    public static final String GAME_ID_CANNOT_BE_BLANK = "경기의 id 를 입력해주세요.";
+    public static final String TEAM_ID_CANNOT_BE_BLANK = "팀의 id 를 입력해주세요.";
     public static final String CONTENT_OF_COMMENT_CANNOT_BE_BLANK = "댓글의 내용을 입력해주세요.";
 }
