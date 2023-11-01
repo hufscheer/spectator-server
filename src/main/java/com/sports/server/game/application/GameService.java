0d19a7afc7d641670deea9586bf92d8c87c465c5
Jin409 <sualng123@gmail.com>
2023-11-01 16:28:10 +0900

    [STYLE] 사용되지 않는 의존성 주입 삭제 #34

--- src/main/java/com/sports/server/game/application/GameService.java
+++ src/main/java/com/sports/server/game/application/GameService.java
@@ -4,17 +4,13 @@ import com.sports.server.common.exception.ExceptionMessages;
 import com.sports.server.common.exception.NotFoundException;
 import com.sports.server.game.domain.Game;
 import com.sports.server.game.domain.GameRepository;
-import com.sports.server.game.dto.request.GameRegisterRequestDto;
 import com.sports.server.game.dto.response.GameDetailResponseDto;
 import com.sports.server.game.dto.response.GameResponseDto;
-import com.sports.server.team.application.TeamService;
-import com.sports.server.team.domain.Team;
+import java.time.LocalDateTime;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDateTime;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -22,7 +18,6 @@ import java.util.List;
 public class GameService {
 
     private final GameRepository gameRepository;
-    private final TeamService teamService;
 
     public GameDetailResponseDto getOneGame(final Long gameId) {
         Game game = findGameWithId(gameId);
@@ -30,11 +25,13 @@ public class GameService {
     }
 
     public Game findGameWithId(final Long gameId) {
-        return gameRepository.findById(gameId).orElseThrow(() -> new NotFoundException(ExceptionMessages.GAME_NOT_FOUND_EXCEPTION));
+        return gameRepository.findById(gameId)
+                .orElseThrow(() -> new NotFoundException(ExceptionMessages.GAME_NOT_FOUND_EXCEPTION));
     }
 
     public List<GameResponseDto> getAllGames() {
         LocalDateTime sevenDaysAgo = LocalDateTime.now().minusDays(7);
-        return gameRepository.findAllByStartTimeIsAfterOrderByStartTime(sevenDaysAgo).stream().map(GameResponseDto::new).toList();
+        return gameRepository.findAllByStartTimeIsAfterOrderByStartTime(sevenDaysAgo).stream().map(GameResponseDto::new)
+                .toList();
     }
 }
