d4e3642208f9ff990ec2a24b271588404d851cd6
Jin409 <sualng123@gmail.com>
2023-11-01 13:20:41 +0900

    [REFACTOR] 기획의도에 맞춰 Team 엔티티 수정 #34

--- src/main/java/com/sports/server/team/domain/Team.java
+++ src/main/java/com/sports/server/team/domain/Team.java
@@ -1,12 +1,20 @@
 package com.sports.server.team.domain;
 
-import jakarta.persistence.*;
+import com.sports.server.member.domain.Member;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Team {
 
     @Id
@@ -19,4 +27,7 @@ public class Team {
     @Column(name = "logo_image_url", nullable = false)
     private String logoImageUrl;
 
+    @ManyToOne
+    @JoinColumn(name = "administrator_id")
+    private Member administrator;
 }
