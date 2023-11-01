5fbb9b9c835486a1ce1089dc62637072b00f0157
Jin409 <sualng123@gmail.com>
2023-11-02 01:18:25 +0900

    [DEL] 사용되지 않는 경기 저장 메서드 삭제 #34

--- src/main/java/com/sports/server/game/domain/GameRepository.java
+++ src/main/java/com/sports/server/game/domain/GameRepository.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 import org.springframework.data.repository.Repository;
 
 public interface GameRepository extends Repository<Game, Long> {
-    Long save(final Game game);
 
     Optional<Game> findById(final Long id);
 
