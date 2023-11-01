82b7693fc650fc5cbfd9856144835bdbe88a755d
Jin409 <sualng123@gmail.com>
2023-11-01 13:21:29 +0900

    [ADD] GameTeam 엔티티 추가 #34

---
+++ src/main/java/com/sports/server/game/domain/GameTeam.java
@@ -0,0 +1,35 @@
+package com.sports.server.game.domain;
+
+import com.sports.server.team.domain.Team;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class GameTeam {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "game_id")
+    private Game game;
+
+    @ManyToOne
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    @Column(name = "cheer_count", nullable = false)
+    private int cheerCount;
+
+    @Column(name = "score", nullable = false)
+    private int score;
+}
