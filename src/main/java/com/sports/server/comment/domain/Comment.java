9d796bbe357b259aa1d393b94d27796931165638
Jin409 <sualng123@gmail.com>
2023-11-01 15:58:25 +0900

    [REFACTOR] 기획 의도에 맞춰 Comment 엔티티 수정 #34

--- src/main/java/com/sports/server/comment/domain/Comment.java
+++ src/main/java/com/sports/server/comment/domain/Comment.java
@@ -1,10 +1,19 @@
 package com.sports.server.comment.domain;
 
-import com.sports.server.game.domain.Game;
-import jakarta.persistence.*;
-import lombok.*;
-
+import com.sports.server.game.domain.GameTeam;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
@@ -26,12 +35,12 @@ public class Comment {
     @Column(name = "is_blocked", nullable = false)
     private boolean isBlocked;
 
+    @ManyToOne
+    @JoinColumn(name = "game_team_id")
+    private GameTeam gameTeam;
+
     public boolean getIsBlocked() {
         return isBlocked;
     }
-
-    @ManyToOne
-    @JoinColumn(name = "game_id")
-    private Game game;
 
 }
