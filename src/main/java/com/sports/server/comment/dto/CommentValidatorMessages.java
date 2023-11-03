06273da22eb84a8d56ce2daac5954a6698b52ac4
Jin409 <sualng123@gmail.com>
2023-11-04 00:14:08 +0900

    [REFACTOR] 도메인에 속하도록 유효성 검사 메시지 위치를 수정 #36

---
+++ src/main/java/com/sports/server/comment/dto/CommentValidatorMessages.java
@@ -0,0 +1,6 @@
+package com.sports.server.comment.dto;
+
+public class CommentValidatorMessages {
+    public static final String TEAM_ID_CANNOT_BE_BLANK = "팀의 id 를 입력해주세요.";
+    public static final String CONTENT_OF_COMMENT_CANNOT_BE_BLANK = "댓글의 내용을 입력해주세요.";
+}
