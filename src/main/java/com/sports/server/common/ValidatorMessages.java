0813e50fd96bce22f32f54f1bdb4759ed96cb4e2
Jin409 <sualng123@gmail.com>
2023-11-04 00:13:46 +0900

    [DEL] common 에 속한 유효성 검사 메시지 클래스 삭제 #36

--- src/main/java/com/sports/server/common/ValidatorMessages.java
+++
@@ -1,7 +0,0 @@
-package com.sports.server.common;
-
-public class ValidatorMessages {
-
-    public static final String TEAM_ID_CANNOT_BE_BLANK = "팀의 id 를 입력해주세요.";
-    public static final String CONTENT_OF_COMMENT_CANNOT_BE_BLANK = "댓글의 내용을 입력해주세요.";
-}
