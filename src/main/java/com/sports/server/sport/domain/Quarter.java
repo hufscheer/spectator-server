6ed5c2ce62059e94a5ae657d3c8781d932e46fdc
Jin409 <sualng123@gmail.com>
2023-11-04 17:22:01 +0900

    [ADD] Quarter 가 Sport 를 단방향 조인하도록 필드 추가 #36

--- src/main/java/com/sports/server/sport/domain/Quarter.java
+++ src/main/java/com/sports/server/sport/domain/Quarter.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
@@ -20,4 +22,8 @@ public class Quarter {
 
     @Column(name = "name", nullable = false)
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "sports_id")
+    private Sport sport;
 }
