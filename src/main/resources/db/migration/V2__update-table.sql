f0fe00a6d6e77eac8ad4860e021dab5931138e10
does <ldk980130@gmail.com>
2023-11-07 20:58:02 +0900

    [FEAT] comments.game_team_id에 인덱스 추가

--- src/main/resources/db/migration/V2__update-table.sql
+++ src/main/resources/db/migration/V2__update-table.sql
@@ -1,5 +1,8 @@
 -- comments -> game_teams 외래키 제약 제거
 ALTER TABLE comments DROP FOREIGN KEY FK_COMMENTS_ON_GAME_TEAM;
 
+-- comments.game_team_id에 인덱스 추가
+CREATE INDEX IDX_COMMENTS_ON_GAME_TEAM_ID ON comments (game_team_id);
+
 -- games에 name 추가
 ALTER TABLE games ADD name VARCHAR(255) NOT NULL;
