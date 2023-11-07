718334b649ba35c95d08d2535d18f0eb20fb7d4d
does <ldk980130@gmail.com>
2023-11-07 22:48:26 +0900

    [STYLE] 유니크 제약 주석 추가

--- src/main/resources/db/migration/V2__update-table.sql
+++ src/main/resources/db/migration/V2__update-table.sql
@@ -7,7 +7,6 @@ CREATE INDEX IDX_COMMENTS_ON_GAME_TEAM_ID ON comments (game_team_id);
 -- games에 name, state 추가
 ALTER TABLE games ADD name VARCHAR(255) NOT NULL;
 ALTER TABLE games ADD state VARCHAR(255) NOT NULL;
-ALTER TABLE reports ADD CONSTRAINT uc_reports_comment UNIQUE (comment_id);
 
 -- meber_id -> administrator_id 컬럼명 변경
 ALTER TABLE games CHANGE member_id administrator_id BIGINT;
@@ -15,3 +14,6 @@ ALTER TABLE games CHANGE member_id administrator_id BIGINT;
 -- reports 상태 칼럼 추가
 ALTER TABLE reports ADD COLUMN state VARCHAR(255);
 ALTER TABLE reports DROP COLUMN is_valid;
+
+-- 댓글과 신고 1대1 매핑
+ALTER TABLE reports ADD CONSTRAINT uc_reports_comment UNIQUE (comment_id);
