bc06bbbe6878380d1c56f0775c3a216b1c6d6783
does <ldk980130@gmail.com>
2023-11-12 20:27:47 +0900

    [TEST] ReportEventHandlerTest 작성 #48

---
+++ src/test/java/com/sports/server/report/infrastructure/ReportEventHandlerTest.java
@@ -0,0 +1,75 @@
+package com.sports.server.report.infrastructure;
+
+import com.sports.server.comment.domain.Comment;
+import com.sports.server.report.domain.Report;
+import com.sports.server.report.domain.ReportEvent;
+import com.sports.server.support.ServiceTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.jdbc.Sql;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
+
+@Sql(scripts = "/report-fixture.sql")
+class ReportEventHandlerTest extends ServiceTest {
+
+    @Autowired
+    private ReportEventHandler reportEventHandler;
+
+    @DisplayName("신고 이벤트가 발생하면")
+    @Nested
+    class ReportEventHandleTest {
+
+        private Report report;
+
+        private static final String COMMENT_CONTENT = "신고될 댓글";
+        private static final Long COMMENT_ID = 1L;
+        private static final Long REPORT_ID = 1L;
+
+        @BeforeEach
+        void init() {
+            report = mock(Report.class);
+            Comment comment = mock(Comment.class);
+            given(report.getComment()).willReturn(comment);
+            given(comment.getContent()).willReturn(COMMENT_CONTENT);
+            given(comment.getId()).willReturn(COMMENT_ID);
+            given(report.getId()).willReturn(REPORT_ID);
+        }
+
+        @Test
+        void 아직_검사가_안된_신고는_검사를_요청한다() {
+            // given
+            given(report.isUnchecked()).willReturn(true);
+            given(reportCheckClient.check(any()))
+                    .willReturn(ResponseEntity.ok().build());
+
+            // when
+            reportEventHandler.handle(new ReportEvent(report));
+
+            // then
+            verify(reportCheckClient).check(
+                    new ReportCheckRequest(COMMENT_CONTENT, COMMENT_ID, REPORT_ID)
+            );
+        }
+
+        @Test
+        void 이미_검사된_신고는_검사를_요청하지_않는다() {
+            // given
+            given(report.isUnchecked()).willReturn(false);
+            given(reportCheckClient.check(any()))
+                    .willReturn(ResponseEntity.ok().build());
+
+            // when
+            reportEventHandler.handle(new ReportEvent(report));
+
+            // then
+            verify(reportCheckClient, never()).check(any());
+        }
+    }
+}
--- src/test/java/com/sports/server/support/ServiceTest.java
+++ src/test/java/com/sports/server/support/ServiceTest.java
@@ -2,13 +2,24 @@ package com.sports.server.support;
 
 import com.sports.server.report.infrastructure.ReportCheckClient;
 import com.sports.server.support.isolation.DatabaseIsolation;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.ResponseEntity;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 
 @SpringBootTest
 @DatabaseIsolation
 public class ServiceTest {
 
     @MockBean
-    private ReportCheckClient reportCheckClient;
+    protected ReportCheckClient reportCheckClient;
+
+    @BeforeEach
+    void init() {
+        given(reportCheckClient.check(any()))
+                .willReturn(ResponseEntity.ok().build());
+    }
 }
