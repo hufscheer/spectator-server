f59928373e49b00882a9c44bffa8d5ce6c00efa3
Jin409 <sualng123@gmail.com>
2023-11-12 16:52:19 +0900

    [STYLE] 가독성을 향상을 위해 한줄에 한 점만 찍도록 수정 #44

--- src/main/java/com/sports/server/game/application/GameService.java
+++ src/main/java/com/sports/server/game/application/GameService.java
@@ -6,9 +6,9 @@ import com.sports.server.game.domain.Game;
 import com.sports.server.game.domain.GameRepository;
 import com.sports.server.game.domain.GameTeam;
 import com.sports.server.game.domain.GameTeamRepository;
-import com.sports.server.game.dto.response.GameTeamCheerResponseDto;
 import com.sports.server.game.dto.response.GameDetailResponseDto;
 import com.sports.server.game.dto.response.GameResponseDto;
+import com.sports.server.game.dto.response.GameTeamCheerResponseDto;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -41,6 +41,8 @@ public class GameService {
     public List<GameTeamCheerResponseDto> getCheerCountOfGameTeams(final Long gameId) {
         Game game = findGameWithId(gameId);
         List<GameTeam> gameTeams = gameTeamRepository.findAllByGame(game);
-        return gameTeams.stream().map(GameTeamCheerResponseDto::new).toList();
+        return gameTeams.stream()
+                .map(GameTeamCheerResponseDto::new)
+                .toList();
     }
 }
