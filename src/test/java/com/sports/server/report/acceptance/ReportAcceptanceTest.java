0044b0a394750414e15118446e245f438455b566
does <ldk980130@gmail.com>
2023-11-12 18:16:15 +0900

    [TEST] 존재하지 않는 댓글을 신고하는 경우 테스트 작성 #48

--- src/test/java/com/sports/server/report/acceptance/ReportAcceptanceTest.java
+++ src/test/java/com/sports/server/report/acceptance/ReportAcceptanceTest.java
@@ -5,6 +5,8 @@ import com.sports.server.support.AcceptanceTest;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -13,24 +15,47 @@ import org.springframework.test.context.jdbc.Sql;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Sql(scripts = "/report-fixture.sql")
-public class ReportAcceptanceTest extends AcceptanceTest {
+class ReportAcceptanceTest extends AcceptanceTest {
 
-    @Test
-    void 댓글을_신고한다() {
-        // given
-        Long commentId = 1L;
-        ReportRequest request = new ReportRequest(commentId);
+    @DisplayName("신고를 할 때")
+    @Nested
+    class ReportTest {
+        @Test
+        void 존재하는_댓글이면_신고한다() {
+            // given
+            Long existComment = 1L;
+            ReportRequest request = new ReportRequest(existComment);
 
-        // when
-        ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .when()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(request)
-                .post("/reports")
-                .then().log().all()
-                .extract();
+            // when
+            ExtractableResponse<Response> response = RestAssured.given().log().all()
+                    .when()
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .body(request)
+                    .post("/reports")
+                    .then().log().all()
+                    .extract();
 
-        // then
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+            // then
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+        }
+
+        @Test
+        void 존재하지_않는_댓글이면_404를_응답한다() {
+            // given
+            Long notExistComment = 100L;
+            ReportRequest request = new ReportRequest(notExistComment);
+
+            // when
+            ExtractableResponse<Response> response = RestAssured.given().log().all()
+                    .when()
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .body(request)
+                    .post("/reports")
+                    .then().log().all()
+                    .extract();
+
+            // then
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.NOT_FOUND.value());
+        }
     }
 }
