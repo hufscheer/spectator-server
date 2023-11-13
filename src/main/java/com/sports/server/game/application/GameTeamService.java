0bd58d68238f253a7ff7ae5ba1f21ce4f82b2370
Jin409 <sualng123@gmail.com>
2023-11-13 16:11:22 +0900

    [FEAT] 응원횟수를 증가시키는 기능 구현 #47

--- src/main/java/com/sports/server/game/application/GameTeamService.java
+++ src/main/java/com/sports/server/game/application/GameTeamService.java
@@ -4,6 +4,7 @@ import com.sports.server.common.exception.NotFoundException;
 import com.sports.server.game.domain.Game;
 import com.sports.server.game.domain.GameTeam;
 import com.sports.server.game.domain.GameTeamRepository;
+import com.sports.server.game.dto.request.GameTeamCheerRequestDto;
 import com.sports.server.game.dto.response.GameTeamCheerResponseDto;
 import com.sports.server.game.exception.GameExceptionMessages;
 import java.util.List;
@@ -30,5 +31,12 @@ public class GameTeamService {
     private GameTeam getGameTeamWithId(final Long gameTeamId) {
         return gameTeamRepository.findById(gameTeamId)
                 .orElseThrow(() -> new NotFoundException(GameExceptionMessages.GAME_TEAM_NOT_FOUND_EXCEPTION));
+    }
+
+    @Transactional
+    public void updateCheerCount(final Long gameId, final GameTeamCheerRequestDto cheerRequestDto) {
+        gameServiceUtils.findGameWithId(gameId);
+        getGameTeamWithId(cheerRequestDto.gameTeamId());
+        gameTeamRepository.updateCheerCount(cheerRequestDto.gameTeamId(), cheerRequestDto.cheerCount());
     }
 }
--- src/main/java/com/sports/server/game/presentation/GameController.java
+++ src/main/java/com/sports/server/game/presentation/GameController.java
@@ -2,14 +2,18 @@ package com.sports.server.game.presentation;
 
 import com.sports.server.game.application.GameService;
 import com.sports.server.game.application.GameTeamService;
+import com.sports.server.game.dto.request.GameTeamCheerRequestDto;
 import com.sports.server.game.dto.response.GameDetailResponseDto;
 import com.sports.server.game.dto.response.GameResponseDto;
 import com.sports.server.game.dto.response.GameTeamCheerResponseDto;
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,5 +40,12 @@ public class GameController {
             @PathVariable final Long gameId
     ) {
         return ResponseEntity.ok(gameTeamService.getCheerCountOfGameTeams(gameId));
+    }
+
+    @PostMapping("/{gameId}/cheer")
+    public ResponseEntity<List<GameTeamCheerResponseDto>> updateCheerCount(@PathVariable final Long gameId,
+                                                                           @RequestBody @Valid GameTeamCheerRequestDto cheerRequestDto) {
+        gameTeamService.updateCheerCount(gameId, cheerRequestDto);
+        return ResponseEntity.ok(null);
     }
 }
