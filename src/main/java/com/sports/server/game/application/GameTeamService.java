68649822214aeded230767eea023467e94d478a4
Jin409 <sualng123@gmail.com>
2023-11-13 16:27:10 +0900

    [REFACTOR] 클래스명 변경에 따른 리팩토링 #47

--- src/main/java/com/sports/server/game/application/GameTeamService.java
+++ src/main/java/com/sports/server/game/application/GameTeamService.java
@@ -6,7 +6,7 @@ import com.sports.server.game.domain.GameTeam;
 import com.sports.server.game.domain.GameTeamRepository;
 import com.sports.server.game.dto.request.GameTeamCheerRequestDto;
 import com.sports.server.game.dto.response.GameTeamCheerResponseDto;
-import com.sports.server.game.exception.GameExceptionMessages;
+import com.sports.server.game.exception.GameErrorMessages;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -30,7 +30,7 @@ public class GameTeamService {
 
     private GameTeam getGameTeamWithId(final Long gameTeamId) {
         return gameTeamRepository.findById(gameTeamId)
-                .orElseThrow(() -> new NotFoundException(GameExceptionMessages.GAME_TEAM_NOT_FOUND_EXCEPTION));
+                .orElseThrow(() -> new NotFoundException(GameErrorMessages.GAME_TEAM_NOT_FOUND_EXCEPTION));
     }
 
     @Transactional
