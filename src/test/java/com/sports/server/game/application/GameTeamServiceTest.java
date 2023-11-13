665efecaec2531892d81a3a75da40600aba77720
Jin409 <sualng123@gmail.com>
2023-11-13 16:48:27 +0900

    [TEST] 경기에 참여하지 않는 팀에 대해서 요청했을 때 예외가 발생하는지 확인하는 테스트 추가 #47

---
+++ src/test/java/com/sports/server/game/application/GameTeamServiceTest.java
@@ -0,0 +1,35 @@
+package com.sports.server.game.application;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import com.sports.server.common.exception.CustomException;
+import com.sports.server.game.dto.request.GameTeamCheerRequestDto;
+import com.sports.server.support.isolation.DatabaseIsolation;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+
+@SpringBootTest
+@DatabaseIsolation
+@Sql(scripts = "/game-fixture.sql")
+public class GameTeamServiceTest {
+
+    @Autowired
+    private GameTeamService gameTeamService;
+
+    @Test
+    void 경기에_참여하지_않는_팀을_응원하면_예외가_발생한다() {
+
+        //given
+        Long gameId = 1L;
+        Long gameTeamId = 3L;
+        GameTeamCheerRequestDto cheerRequestDto = new GameTeamCheerRequestDto(gameTeamId, 1);
+
+        //when&then
+        assertThrows(CustomException.class, () -> {
+            gameTeamService.updateCheerCount(gameId, cheerRequestDto);
+        });
+
+    }
+}
