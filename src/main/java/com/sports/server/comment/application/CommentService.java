ee7eb538777e717da781ea24a5ae109687945c67
Jin409 <sualng123@gmail.com>
2023-11-15 11:47:12 +0900

    [FEAT] 욕설이 포함된 댓글 저장 시도 시에 예외가 던져지도록 하는 기능 구현 #53

--- src/main/java/com/sports/server/comment/application/CommentService.java
+++ src/main/java/com/sports/server/comment/application/CommentService.java
@@ -1,14 +1,18 @@
 package com.sports.server.comment.application;
 
+import static com.sports.server.comment.exception.CommentErrorMessages.COMMENT_CONTAINS_BAD_WORD;
+
 import com.sports.server.comment.domain.Comment;
 import com.sports.server.comment.domain.CommentRepository;
 import com.sports.server.comment.dto.request.CommentRequestDto;
 import com.sports.server.comment.dto.response.CommentResponseDto;
+import com.sports.server.common.exception.CustomException;
+import com.vane.badwordfiltering.BadWordFiltering;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -16,11 +20,19 @@ import java.util.List;
 public class CommentService {
 
     private final CommentRepository commentRepository;
+    private final BadWordFiltering badWordFiltering;
 
     @Transactional
     public void register(final CommentRequestDto commentRequestDto) {
-        Comment comment = new Comment(commentRequestDto.getContent(), commentRequestDto.getGameTeamId());
+        validateContent(commentRequestDto.content());
+        Comment comment = new Comment(commentRequestDto.content(), commentRequestDto.gameTeamId());
         commentRepository.save(comment);
+    }
+
+    private void validateContent(final String content) {
+        if (badWordFiltering.blankCheck(content)) {
+            throw new CustomException(HttpStatus.BAD_REQUEST, COMMENT_CONTAINS_BAD_WORD);
+        }
     }
 
     public List<CommentResponseDto> getAllCommentsWithGameId(final Long gameId) {
---
+++ src/main/java/com/sports/server/comment/exception/CommentErrorMessages.java
@@ -0,0 +1,5 @@
+package com.sports.server.comment.exception;
+
+public class CommentErrorMessages {
+    public static final String COMMENT_CONTAINS_BAD_WORD = "댓글에 욕설이 포함되어 있어 저장할 수 없습니다.";
+}
