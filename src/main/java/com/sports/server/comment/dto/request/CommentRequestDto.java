dfbda80923dac84a582266cc74386bd016392575
Jin409 <sualng123@gmail.com>
2023-11-15 11:45:42 +0900

    [REFACTOR] RequestDto 를 record 로 수정 #53

--- src/main/java/com/sports/server/comment/dto/request/CommentRequestDto.java
+++ src/main/java/com/sports/server/comment/dto/request/CommentRequestDto.java
@@ -1,19 +1,7 @@
 package com.sports.server.comment.dto.request;
 
-import com.sports.server.comment.domain.Comment;
-import com.sports.server.comment.dto.CommentValidatorMessages;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Getter
-@NoArgsConstructor
-public class CommentRequestDto {
-
-    @NotBlank(message = CommentValidatorMessages.CONTENT_OF_COMMENT_CANNOT_BE_BLANK)
-    private String content;
-
-    @NotNull(message = CommentValidatorMessages.TEAM_ID_CANNOT_BE_BLANK)
-    private Long gameTeamId;
+public record CommentRequestDto(
+        String content,
+        Long gameTeamId
+) {
 }
