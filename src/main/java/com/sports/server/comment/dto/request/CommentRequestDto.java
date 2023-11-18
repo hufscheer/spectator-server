69ccc794a5981c944681d696e32fa45064615518
Jin409 <sualng123@gmail.com>
2023-11-19 00:14:06 +0900

    [ADD] Request dto 에 validation 추가 #53

--- src/main/java/com/sports/server/comment/dto/request/CommentRequestDto.java
+++ src/main/java/com/sports/server/comment/dto/request/CommentRequestDto.java
@@ -1,7 +1,9 @@
 package com.sports.server.comment.dto.request;
 
+import lombok.NonNull;
+
 public record CommentRequestDto(
-        String content,
-        Long gameTeamId
+        @NonNull String content,
+        @NonNull Long gameTeamId
 ) {
 }
