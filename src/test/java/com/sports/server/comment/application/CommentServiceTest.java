75039d55c03234b36b2d2668cb9158db9125df58
Jin409 <sualng123@gmail.com>
2023-11-20 15:39:32 +0900

    [TEST] 욕설 필터링 테스트 추가 #53

--- src/test/java/com/sports/server/comment/application/CommentServiceTest.java
+++ src/test/java/com/sports/server/comment/application/CommentServiceTest.java
@@ -1,11 +1,13 @@
 package com.sports.server.comment.application;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import com.sports.server.comment.dto.request.CommentRequestDto;
 import com.sports.server.common.exception.CustomException;
 import com.sports.server.support.ServiceTest;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class CommentServiceTest extends ServiceTest {
@@ -13,18 +15,30 @@ public class CommentServiceTest extends ServiceTest {
     @Autowired
     private CommentService commentService;
 
-    @Test
-    void 욕설이_포함된_댓글을_저장하려고_하면_예외가_발생한다() {
+    @ParameterizedTest
+    @ValueSource(strings = {"ㅅㅂ", "개새", "ㅆㅂ"})
+    void 욕설이_포함된_댓글을_저장하려고_하면_예외가_발생한다(String content) {
 
         //given
-        CommentRequestDto commentRequestDto = new CommentRequestDto("ㅅㅂ", 1L);
+        CommentRequestDto commentRequestDto = new CommentRequestDto(content, 1L);
 
         //when & then
-        //when&then
         assertThrows(CustomException.class, () -> {
             commentService.register(commentRequestDto);
         });
 
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"안녕", "파이팅", "할 수 있어!"})
+    void 욕설이_포함되지_않은_댓글은_정상적으로_저장된다(String content) {
+
+        //given
+        CommentRequestDto commentRequestDto = new CommentRequestDto(content, 1L);
+
+        //when & then
+        assertThatCode(() -> commentService.register(commentRequestDto))
+                .doesNotThrowAnyException();
     }
 
 }
