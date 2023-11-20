98a4c8d6b777f3518863c3aba76a1ebd8046d4a8
Jin409 <sualng123@gmail.com>
2023-11-20 15:55:42 +0900

    [REFACTOR] 구현체가 인터페이스를 구현하도록 수정 #53

--- src/main/java/com/sports/server/comment/infra/BadWordFilterImpl.java
+++ src/main/java/com/sports/server/comment/infra/BadWordFilterImpl.java
@@ -1,14 +1,14 @@
-package com.sports.server.comment.util;
+package com.sports.server.comment.infra;
 
 import com.vane.badwordfiltering.BadWordFiltering;
 import org.springframework.stereotype.Component;
 
 @Component
-public class BadWordFilter {
+public class BadWordFilterImpl implements com.sports.server.comment.domain.BadWordFilter {
 
     private final BadWordFiltering badWordFiltering;
 
-    public BadWordFilter() {
+    public BadWordFilterImpl() {
         this.badWordFiltering = new BadWordFiltering();
     }
 
