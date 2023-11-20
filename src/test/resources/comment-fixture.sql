57d23d010156c73d7ab61022cca53efa3c57a0df
Jin409 <sualng123@gmail.com>
2023-11-20 16:26:25 +0900

    [FIX] 댓글 조회 시에 필요한 데이터 추가 #53

--- src/test/resources/comment-fixture.sql
+++ src/test/resources/comment-fixture.sql
@@ -1,10 +1,16 @@
 SET foreign_key_checks = 0;
 
+INSERT INTO games (sport_id, administrator_id, league_id, name, start_time, video_id, quarter_changed_at, game_quarter,
+                   state)
+VALUES (2, 1, 1, '농구 대전', '2023-11-12T10:00:00', 'abc123', '2023-11-12T10:15:00', '1st Quarter', 'SCHEDULED');
+
+INSERT INTO game_teams (game_id, team_id, cheer_count, score)
+VALUES (1, 1, 1, 0), -- 팀 A의 정보
+       (1, 2, 1, 0);
+
 -- Comment 테이블에 데이터를 넣는 INSERT 쿼리
 INSERT INTO `comments` (`created_at`, `content`, `is_blocked`, `game_team_id`)
 VALUES ('2023-01-01 12:30:00', 'Great play!', false, 1),
-       ('2023-01-02 14:45:00', 'What a save!', false, 2),
-       ('2023-01-03 16:00:00', 'Nice teamwork!', true, 3),
-       ('2023-01-04 18:15:00', 'Unbelievable goal!', false, 1);
+       ('2023-01-02 14:45:00', 'What a save!', false, 2);
 
 SET foreign_key_checks = 1;
