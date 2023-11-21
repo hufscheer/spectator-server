7586051a0c96972a6170c5453cac59f89aaf6abf
Jin409 <sualng123@gmail.com>
2023-11-21 20:08:13 +0900

    [TEST] 경기 전체 조회 인수 테스트 추가 #60

--- src/test/java/com/sports/server/game/acceptance/GameAcceptanceTest.java
+++ src/test/java/com/sports/server/game/acceptance/GameAcceptanceTest.java
@@ -1,19 +1,19 @@
 package com.sports.server.game.acceptance;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
 import com.sports.server.game.dto.response.GameDetailResponse;
+import com.sports.server.game.dto.response.GameResponseDto;
 import com.sports.server.support.AcceptanceTest;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import java.time.LocalDateTime;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
-
-import java.time.LocalDateTime;
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
 
 @Sql(scripts = "/game-fixture.sql")
 class GameAcceptanceTest extends AcceptanceTest {
@@ -52,9 +52,62 @@ class GameAcceptanceTest extends AcceptanceTest {
                 () -> assertThat(teams)
                         .filteredOn(team -> team.gameTeamId().equals(2L))
                         .containsExactly(new GameDetailResponse.TeamResponse(
-                                2L,"팀 B",
+                                2L, "팀 B",
                                 "http://example.com/logo_b.png", 2)
                         )
         );
+    }
+
+    @Test
+    void 리그의_경기를_전체_조회한다() {
+
+        // given
+        Long leagueId = 1L;
+
+        // when
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .when()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .get("leagues/{leagueId}/games", leagueId)
+                .then().log().all()
+                .extract();
+
+        //then
+        List<GameResponseDto> games = toResponses(response, GameResponseDto.class);
+        assertAll(
+                () -> assertThat(games).hasSize(2),
+                () -> assertThat(games)
+                        .filteredOn(game -> game.id().equals(1L))
+                        .containsExactly(
+                                new GameResponseDto(
+                                        1L, LocalDateTime.of(2023, 11, 12, 10, 0, 0),
+                                        "1st Quarter", "농구 대전",
+                                        List.of(new GameResponseDto.TeamResponse(
+                                                        1L, "팀 A", "http://example.com/logo_a.png", 1
+                                                ),
+                                                new GameResponseDto.TeamResponse(
+                                                        2L, "팀 B", "http://example.com/logo_b.png", 2)),
+                                        "농구"
+                                )
+                        ),
+                () -> assertThat(games)
+                        .filteredOn(game -> game.id().equals(2L))
+                        .containsExactly(
+                                new GameResponseDto(
+                                        2L, LocalDateTime.of(2023, 11, 13, 14, 30, 0),
+                                        "2nd Quarter", "롤 챔피언스",
+                                        List.of(new GameResponseDto.TeamResponse(
+                                                        3L, "팀 B", "http://example.com/logo_b.png", 0),
+                                                new GameResponseDto.TeamResponse(
+                                                        4L, "팀 C", "http://example.com/logo_c.png", 0)
+
+                                        ),
+
+                                        "농구"
+                                )
+
+                        )
+        );
+
     }
 }
--- src/test/resources/game-fixture.sql
+++ src/test/resources/game-fixture.sql
@@ -3,15 +3,15 @@ SET foreign_key_checks = 0;
 -- 경기
 INSERT INTO games (sport_id, administrator_id, league_id, name, start_time, video_id, quarter_changed_at, game_quarter,
                    state)
-VALUES (2, 1, 1, '농구 대전', '2023-11-12T10:00:00', 'abc123', '2023-11-12T10:15:00', '1st Quarter', 'SCHEDULED');
+VALUES (1, 1, 1, '농구 대전', '2023-11-12T10:00:00', 'abc123', '2023-11-12T10:15:00', '1st Quarter', 'SCHEDULED');
 
 INSERT INTO games (sport_id, administrator_id, league_id, name, start_time, video_id, quarter_changed_at, game_quarter,
                    state)
-VALUES (3, 2, 1, '롤 챔피언스', '2023-11-13T14:30:00', 'def456', '2023-11-13T15:00:00', '2nd Quarter', 'SCHEDULED');
+VALUES (1, 2, 1, '롤 챔피언스', '2023-11-13T14:30:00', 'def456', '2023-11-13T15:00:00', '2nd Quarter', 'SCHEDULED');
 
 INSERT INTO games (sport_id, administrator_id, league_id, name, start_time, video_id, quarter_changed_at, game_quarter,
                    state)
-VALUES (4, 3, 2, '루미큐브 대회', '2023-11-14T18:45:00', 'ghi789', '2023-11-14T19:00:00', '3rd Quarter', 'SCHEDULED');
+VALUES (1, 3, 2, '루미큐브 대회', '2023-11-14T18:45:00', 'ghi789', '2023-11-14T19:00:00', '3rd Quarter', 'SCHEDULED');
 
 -- 팀
 INSERT INTO teams (name, logo_image_url, administrator_id, organization_id, league_id)
@@ -23,11 +23,9 @@ VALUES ('팀 B', 'http://example.com/logo_b.png', 2, 1, 1);
 INSERT INTO teams (name, logo_image_url, administrator_id, organization_id, league_id)
 VALUES ('팀 C', 'http://example.com/logo_c.png', 3, 2, 2);
 
--- 경기의 팀
--- 농구 대전 (game_id = 1) A팀 vs B팀
-INSERT INTO game_teams (game_id, team_id, cheer_count, score)
-VALUES (1, 1, 1, 1), -- 팀 A의 정보
-       (1, 2, 2, 2); -- 팀 B의 정보
+-- 스포츠
+INSERT INTO sports(id, name)
+VALUES (1, '농구');
 
 -- 농구 대전(game_id = 1) A팀 선수
 INSERT INTO game_team_players (id, game_team_id, name, description)
@@ -46,6 +44,10 @@ VALUES (6, 2, '선수6', '센터'),
        (9, 2, '선수9', '포인트 가드'),
        (10, 2, '선수10', '스몰 포워드');
 
+-- 농구 대전 (game_id = 1) A팀 vs B팀
+INSERT INTO game_teams (game_id, team_id, cheer_count, score)
+VALUES (1, 1, 1, 1), -- 팀 A의 정보
+       (1, 2, 2, 2);
 
 -- 롤 챔피언스 (game_id = 2) B팀 vs C팀
 INSERT INTO game_teams (game_id, team_id, cheer_count, score)
@@ -56,5 +58,8 @@ VALUES (2, 2, 1, 0),
 INSERT INTO game_teams (game_id, team_id, cheer_count, score)
 VALUES (3, 1, 1, 0),
        (3, 3, 1, 0);
+
+INSERT INTO leagues (id, administrator_id, organization_id, name, start_at, end_at, is_deleted)
+VALUES (1, 1, 1, '삼건물 대회', '2023-11-09 00:00:00', '2023-11-20 00:00:00', false);
 
 SET foreign_key_checks = 1;
