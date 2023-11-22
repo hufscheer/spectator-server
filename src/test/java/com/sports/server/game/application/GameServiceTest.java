533afbdb271acf04de8f304f260025e013df861d
Jin409 <sualng123@gmail.com>
2023-11-22 15:30:11 +0900

    [TEST] 페이징 테스트 추가 #60

--- src/test/java/com/sports/server/game/application/GameServiceTest.java
+++ src/test/java/com/sports/server/game/application/GameServiceTest.java
@@ -4,11 +4,13 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.sports.server.common.exception.CustomException;
+import com.sports.server.game.dto.request.PageRequestDto;
 import com.sports.server.game.dto.response.GameResponseDto;
 import com.sports.server.support.ServiceTest;
 import java.time.LocalDateTime;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.jdbc.Sql;
@@ -18,17 +20,21 @@ public class GameServiceTest extends ServiceTest {
 
     @Autowired
     GameService gameService;
+    private final Long validLeagueId = 1L;
+    private final int size = 5;
+    private final PageRequestDto pageRequestDto = new PageRequestDto(null, size);
+    private final List<Long> sportIds = List.of(1L, 2L);
+    private final String stateValue = "SCHEDULED";
 
     @Test
     void 존재하지_않는_게임_상태를_조회할_때_예외가_발생한다() {
 
         //given
-        Long leagueId = 1L;
         String invalidStateValue = "INVALID";
-        List<Long> sportIds = List.of(1L);
 
         // when
-        assertThrows(CustomException.class, () -> gameService.getAllGames(leagueId, invalidStateValue, sportIds));
+        assertThrows(CustomException.class,
+                () -> gameService.getAllGames(validLeagueId, invalidStateValue, sportIds, pageRequestDto));
 
     }
 
@@ -36,12 +42,10 @@ public class GameServiceTest extends ServiceTest {
     void 날짜순으로_경기들이_반환된다() {
 
         //given
-        Long leagueId = 1L;
-        String stateValue = "scheduled";
         List<Long> sportIds = List.of(1L);
 
         //when
-        List<GameResponseDto> games = gameService.getAllGames(leagueId, stateValue, sportIds);
+        List<GameResponseDto> games = gameService.getAllGames(validLeagueId, stateValue, sportIds, pageRequestDto);
 
         //then
         LocalDateTime startTimeOfFirstGame = games.get(0).startTime();
@@ -56,12 +60,10 @@ public class GameServiceTest extends ServiceTest {
     void 날짜가_같은_경우_경기의_pk순으로_경기들이_반환된다() {
 
         //given
-        Long leagueId = 1L;
-        String stateValue = "scheduled";
         List<Long> sportIds = List.of(1L);
 
         //when
-        List<GameResponseDto> games = gameService.getAllGames(leagueId, stateValue, sportIds);
+        List<GameResponseDto> games = gameService.getAllGames(validLeagueId, stateValue, sportIds, pageRequestDto);
 
         //then
         GameResponseDto firstGame = games.get(0);
@@ -82,31 +84,24 @@ public class GameServiceTest extends ServiceTest {
     @Test
     void 스포츠_아이디가_쿼리_스트링으로_조회되지_않는_경우_전체가_반환된다() {
 
-        //given
-        Long leagueId = 1L;
-        String stateValue = "scheduled";
-
         //when
-        List<GameResponseDto> games = gameService.getAllGames(leagueId, stateValue, null);
+        List<GameResponseDto> games = gameService.getAllGames(validLeagueId, stateValue, null, pageRequestDto);
 
         //then
         Assertions.assertEquals(
-                games.size(), 3
+                games.size(), size
         );
     }
 
     @Test
     void 리그_아이디가_쿼리_스트링으로_조회되지_않는_경우_전체가_반환된다() {
 
-        //given
-        String stateValue = "scheduled";
-
         //when
-        List<GameResponseDto> games = gameService.getAllGames(null, stateValue, null);
+        List<GameResponseDto> games = gameService.getAllGames(null, stateValue, null, pageRequestDto);
 
         //then
         Assertions.assertEquals(
-                games.size(), 4
+                games.size(), size
         );
 
     }
@@ -114,11 +109,8 @@ public class GameServiceTest extends ServiceTest {
     @Test
     void 하나의_경기에서_팀이_아이디_순으로_반환된다() {
 
-        //given
-        String stateValue = "scheduled";
-
         //when
-        List<GameResponseDto> games = gameService.getAllGames(null, stateValue, null);
+        List<GameResponseDto> games = gameService.getAllGames(null, stateValue, null, pageRequestDto);
 
         //then
         Assertions.assertTrue(
@@ -126,4 +118,45 @@ public class GameServiceTest extends ServiceTest {
         );
 
     }
+
+    @DisplayName("경기들을 페이징을 이용해서 조회할 때")
+    class PagingTest {
+
+        @Test
+        void 세개만_조회한다() {
+
+            // given
+            PageRequestDto pageRequestDto = new PageRequestDto(null, 3);
+
+            //when
+            List<GameResponseDto> games = gameService.getAllGames(null, stateValue, null, pageRequestDto);
+
+            //then
+            Assertions.assertEquals(
+                    games.size(), 3
+            );
+
+
+        }
+
+        @Test
+        void 기본적으로_10개를_조회한다() {
+
+            // given
+            PageRequestDto pageRequestDto = new PageRequestDto(null, null);
+
+            //when
+            List<GameResponseDto> games = gameService.getAllGames(null, stateValue, null, pageRequestDto);
+
+            //then
+            Assertions.assertEquals(
+                    games.size(), 10
+            );
+
+        }
+
+
+    }
+
+
 }
--- src/test/resources/game-fixture.sql
+++ src/test/resources/game-fixture.sql
@@ -18,6 +18,39 @@ INSERT INTO games (sport_id, administrator_id, league_id, name, start_time, vide
                    state)
 VALUES (1, 3, 1, '루미큐브 대회', '2023-11-12T10:00:00', 'ghi789', '2023-11-12T10:15:00', '3rd Quarter', 'SCHEDULED');
 
+INSERT INTO games (sport_id, administrator_id, league_id, name, start_time, video_id, quarter_changed_at, game_quarter,
+                   state)
+VALUES (1, 1, 1, '농구 대전', '2023-11-12T10:00:00', 'abc123', '2023-11-12T10:15:00', '1st Quarter', 'SCHEDULED');
+
+INSERT INTO games (sport_id, administrator_id, league_id, name, start_time, video_id, quarter_changed_at, game_quarter,
+                   state)
+VALUES (1, 2, 1, '롤 복제 1', '2023-11-13T14:30:00', 'def456', '2023-11-13T15:00:00', '2nd Quarter', 'SCHEDULED');
+
+INSERT INTO games (sport_id, administrator_id, league_id, name, start_time, video_id, quarter_changed_at, game_quarter,
+                   state)
+VALUES (1, 3, 1, '루미큐브 복제 1', '2023-11-12T10:00:00', 'ghi789', '2023-11-12T10:15:00', '3rd Quarter', 'SCHEDULED');
+
+INSERT INTO games (sport_id, administrator_id, league_id, name, start_time, video_id, quarter_changed_at, game_quarter,
+                   state)
+VALUES (1, 1, 1, '농구 대전 복제 1', '2023-11-12T10:00:00', 'abc123', '2023-11-12T10:15:00', '1st Quarter', 'SCHEDULED');
+
+INSERT INTO games (sport_id, administrator_id, league_id, name, start_time, video_id, quarter_changed_at, game_quarter,
+                   state)
+VALUES (1, 2, 1, '롤 복제 1', '2023-11-13T14:30:00', 'def456', '2023-11-13T15:00:00', '2nd Quarter', 'SCHEDULED');
+
+INSERT INTO games (sport_id, administrator_id, league_id, name, start_time, video_id, quarter_changed_at, game_quarter,
+                   state)
+VALUES (1, 3, 1, '루미큐브 복제 1', '2023-11-12T10:00:00', 'ghi789', '2023-11-12T10:15:00', '3rd Quarter', 'SCHEDULED');
+
+INSERT INTO games (sport_id, administrator_id, league_id, name, start_time, video_id, quarter_changed_at, game_quarter,
+                   state)
+VALUES (1, 1, 1, '농구 대전 복제 2', '2023-11-12T10:00:00', 'abc123', '2023-11-12T10:15:00', '1st Quarter', 'SCHEDULED');
+
+INSERT INTO games (sport_id, administrator_id, league_id, name, start_time, video_id, quarter_changed_at, game_quarter,
+                   state)
+VALUES (1, 2, 1, '롤 복제 2', '2023-11-13T14:30:00', 'def456', '2023-11-13T15:00:00', '2nd Quarter', 'SCHEDULED');
+
+
 -- 팀
 INSERT INTO teams (name, logo_image_url, administrator_id, organization_id, league_id)
 VALUES ('팀 A', 'http://example.com/logo_a.png', 1, 1, 1);
