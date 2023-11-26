8885072be2589d72ba67b2aeb4bbfcd476e5e95a
Jin409 <sualng123@gmail.com>
2023-11-26 21:23:59 +0900

    [REFACTOR] 댓글 응답 dto record 로 수정 #70

--- src/main/java/com/sports/server/comment/dto/response/CommentResponseDto.java
+++ src/main/java/com/sports/server/comment/dto/response/CommentResponseDto.java
@@ -1,33 +1,19 @@
 package com.sports.server.comment.dto.response;
 
 import com.sports.server.comment.domain.Comment;
-import com.sports.server.common.Constants;
 import java.time.LocalDateTime;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
 
-@Getter
-@NoArgsConstructor
-public class CommentResponseDto {
-
-    private Long id;
-
-    private String content;
-
-    private boolean isBlocked;
-
-    @DateTimeFormat(pattern = Constants.DATE_FORMAT)
-    private LocalDateTime createdAt;
-
-    public boolean getIsBlocked() {
-        return isBlocked;
-    }
-
+public record CommentResponseDto(
+        Long id,
+        String content,
+        Long gameTeamId,
+        boolean isBlocked,
+        LocalDateTime createdAt
+) {
     public CommentResponseDto(final Comment comment) {
-        this.id = comment.getId();
-        this.content = comment.getContent();
-        this.isBlocked = comment.isBlocked();
-        this.createdAt = comment.getCreatedAt();
+        this(
+                comment.getId(), comment.getContent(), comment.getGameTeamId(), comment.isBlocked(),
+                comment.getCreatedAt()
+        );
     }
 }
