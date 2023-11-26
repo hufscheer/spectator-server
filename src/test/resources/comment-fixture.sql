f25b1c298dbc1b9ba1ce28c9d91f667c5b32cfe7
Jin409 <sualng123@gmail.com>
2023-11-26 21:26:02 +0900

    [TEST] 댓글 fixture 수정 #70

--- src/test/resources/comment-fixture.sql
+++ src/test/resources/comment-fixture.sql
@@ -1,12 +1,13 @@
 SET foreign_key_checks = 0;
 
-INSERT INTO games (sport_id, administrator_id, league_id, name, start_time, video_id, quarter_changed_at, game_quarter,
+INSERT INTO games (id, sport_id, administrator_id, league_id, name, start_time, video_id, quarter_changed_at,
+                   game_quarter,
                    state)
-VALUES (2, 1, 1, '농구 대전', '2023-11-12T10:00:00', 'abc123', '2023-11-12T10:15:00', '1st Quarter', 'SCHEDULED');
+VALUES (1, 1, 1, 1, '농구 대전', '2023-11-12T10:00:00', 'abc123', '2023-11-12T10:15:00', '1st Quarter', 'SCHEDULED');
 
-INSERT INTO game_teams (game_id, team_id, cheer_count, score)
-VALUES (1, 1, 1, 0), -- 팀 A의 정보
-       (1, 2, 1, 0);
+INSERT INTO game_teams (id, game_id, team_id, cheer_count, score)
+VALUES (1, 1, 1, 1, 0),
+       (2, 1, 2, 1, 0);
 
 -- Comment 테이블에 데이터를 넣는 INSERT 쿼리
 INSERT INTO `comments` (`created_at`, `content`, `is_blocked`, `game_team_id`)
