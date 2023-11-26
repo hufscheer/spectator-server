60a6c56040f508b8342ca0afbb23e0ad10f002c0
Jin409 <sualng123@gmail.com>
2023-11-26 21:25:51 +0900

    [TEST] AcceptanceTest 의 port 접근 제어자 변경 #70

--- src/test/java/com/sports/server/support/AcceptanceTest.java
+++ src/test/java/com/sports/server/support/AcceptanceTest.java
@@ -1,11 +1,15 @@
 package com.sports.server.support;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
 import com.sports.server.report.infrastructure.ReportCheckClient;
 import com.sports.server.support.config.AsyncTestConfig;
 import com.sports.server.support.isolation.DatabaseIsolation;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -13,18 +17,13 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.ResponseEntity;
 
-import java.util.List;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
-
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Import(AsyncTestConfig.class)
 @DatabaseIsolation
 public class AcceptanceTest {
 
     @LocalServerPort
-    private int port;
+    protected int port;
 
     @MockBean
     protected ReportCheckClient reportCheckClient;
@@ -44,7 +43,7 @@ public class AcceptanceTest {
     }
 
     protected <T> T toResponse(ExtractableResponse<Response> response,
-                                      Class<T> dtoType) {
+                               Class<T> dtoType) {
         return response.jsonPath()
                 .getObject(".", dtoType);
     }
