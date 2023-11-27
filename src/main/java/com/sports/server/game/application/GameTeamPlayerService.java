311b4fbe50a1fd742fd3f5389b53e25e2dcfeed9
Jin409 <sualng123@gmail.com>
2023-11-27 23:56:57 +0900

    [FEAT] 라인업 조회 시 order 찾아 넣는 기능 구현 #75

--- src/main/java/com/sports/server/game/application/GameTeamPlayerService.java
+++ src/main/java/com/sports/server/game/application/GameTeamPlayerService.java
@@ -1,18 +1,19 @@
 package com.sports.server.game.application;
 
+import static java.util.stream.Collectors.groupingBy;
+
 import com.sports.server.game.domain.GameTeam;
 import com.sports.server.game.domain.GameTeamPlayer;
 import com.sports.server.game.domain.GameTeamPlayerRepository;
 import com.sports.server.game.dto.response.GameLineupResponse;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-
-import static java.util.stream.Collectors.groupingBy;
+import java.util.stream.IntStream;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -25,10 +26,16 @@ public class GameTeamPlayerService {
         Map<GameTeam, List<GameTeamPlayer>> groupByTeam = gameTeamPlayerRepository.findPlayersByGameId(gameId)
                 .stream()
                 .collect(groupingBy(GameTeamPlayer::getGameTeam));
-        return groupByTeam.keySet()
-                .stream()
-                .sorted(Comparator.comparingLong(GameTeam::getId))
-                .map(gameTeam -> new GameLineupResponse(gameTeam, groupByTeam.get(gameTeam)))
+
+        return IntStream.range(0, groupByTeam.size())
+                .boxed()
+                .sorted(Comparator.comparingLong(i -> new ArrayList<>(groupByTeam.keySet()).get(i).getId()))
+                .map(i -> {
+                    GameTeam gameTeam = new ArrayList<>(groupByTeam.keySet()).get(i);
+                    List<GameTeamPlayer> players = groupByTeam.get(gameTeam);
+                    return new GameLineupResponse(gameTeam, players, i + 1);
+                })
                 .toList();
     }
+
 }
