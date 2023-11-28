e614028c48f9bb2e29447760d9907c83556a2170
Jin409 <sualng123@gmail.com>
2023-11-28 14:58:18 +0900

    [REFACTOR] 코드 중복 및 각 메서드가 하나의 책임만을 지도록 리팩토링 #75

--- src/main/java/com/sports/server/game/application/GameTeamServiceUtils.java
+++ src/main/java/com/sports/server/game/application/GameTeamServiceUtils.java
@@ -19,28 +19,38 @@ public class GameTeamServiceUtils {
     private final GameTeamRepository gameTeamRepository;
 
     public int calculateOrderOfGameTeam(final Game game, final Long gameTeamId) {
-        List<GameTeam> gameTeams = gameTeamRepository.findAllByGame(game).stream()
-                .sorted(Comparator.comparingLong(GameTeam::getId))
-                .toList();
-
-        return IntStream.range(0, gameTeams.size())
-                .filter(i -> gameTeams.get(i).getId().equals(gameTeamId))
-                .findFirst().orElseThrow(CustomException::new) + 1;
+        List<GameTeam> gameTeams = findSortedGameTeamsByGame(game);
+        return getOrderOfTeam(gameTeamId, gameTeams);
     }
 
     public int calculateOrderOfGameTeam(final Game game, final GameTeam gameTeam) {
-        List<GameTeam> gameTeams = gameTeamRepository.findAllByGame(game).stream()
-                .sorted(Comparator.comparingLong(GameTeam::getId))
-                .toList();
-
-        return gameTeams.indexOf(gameTeam) + 1;
+        List<GameTeam> gameTeams = findSortedGameTeamsByGame(game);
+        return getOrderOfTeam(gameTeam, gameTeams);
     }
 
     public int calculateOrderOfGameTeam(final List<GameTeam> gameTeams, final GameTeam gameTeam) {
-        List<GameTeam> sortedGameTeams = gameTeams.stream()
-                .sorted(Comparator.comparingLong(GameTeam::getId)).toList();
+        List<GameTeam> sortedGameTeams = sortGameTeams(gameTeams);
+        return getOrderOfTeam(gameTeam, sortedGameTeams);
 
-        return sortedGameTeams.indexOf(gameTeam) + 1;
+    }
 
+    private List<GameTeam> sortGameTeams(List<GameTeam> gameTeams) {
+        return gameTeams.stream()
+                .sorted(Comparator.comparingLong(GameTeam::getId))
+                .toList();
+    }
+
+    private List<GameTeam> findSortedGameTeamsByGame(Game game) {
+        return sortGameTeams(gameTeamRepository.findAllByGame(game));
+    }
+
+    private int getOrderOfTeam(GameTeam gameTeam, List<GameTeam> gameTeams) {
+        return gameTeams.indexOf(gameTeam) + 1;
+    }
+
+    private int getOrderOfTeam(Long gameTeamId, List<GameTeam> gameTeams) {
+        return IntStream.range(0, gameTeams.size())
+                .filter(i -> gameTeams.get(i).getId().equals(gameTeamId))
+                .findFirst().orElseThrow(CustomException::new) + 1;
     }
 }
