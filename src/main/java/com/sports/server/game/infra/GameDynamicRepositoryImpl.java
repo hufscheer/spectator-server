c3ea707ab2095e7b2b5aea7b000d1a28ab14f25c
does <ldk980130@gmail.com>
2023-12-01 16:56:19 +0900

    [FIX] start_time과 id를 이어붙인 값으로 정렬 및 커서페이징을 수행하도록 수정 # 84

--- src/main/java/com/sports/server/game/infra/GameDynamicRepositoryImpl.java
+++ src/main/java/com/sports/server/game/infra/GameDynamicRepositoryImpl.java
@@ -1,22 +1,26 @@
 package com.sports.server.game.infra;
 
-import static com.sports.server.game.domain.QGame.game;
-import static com.sports.server.sport.domain.QSport.sport;
-
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.sports.server.common.dto.PageRequestDto;
 import com.sports.server.game.domain.Game;
 import com.sports.server.game.domain.GameDynamicRepository;
 import com.sports.server.game.domain.GameState;
-import java.time.LocalDateTime;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+import static com.sports.server.game.domain.QGame.game;
+import static com.sports.server.sport.domain.QSport.sport;
 
 @Repository
 @RequiredArgsConstructor
 public class GameDynamicRepositoryImpl implements GameDynamicRepository {
+
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     private final JPAQueryFactory jpaQueryFactory;
 
@@ -24,26 +28,35 @@ public class GameDynamicRepositoryImpl implements GameDynamicRepository {
     public List<Game> findAllByLeagueAndStateAndSports(final Long leagueId, final GameState state,
                                                        final List<Long> sportIds, final PageRequestDto pageRequestDto) {
         DynamicBooleanBuilder booleanBuilder = DynamicBooleanBuilder.builder();
-
+        String cursorValue = getCursorValue(pageRequestDto.cursor());
         JPAQuery<Game> gameJPAQuery = jpaQueryFactory
                 .selectFrom(game)
                 .join(game.sport, sport).fetchJoin()
                 .where(booleanBuilder
+                        .and(() -> game.startTime.stringValue().concat(game.id.stringValue()).gt(cursorValue))
                         .and(() -> game.league.id.eq(leagueId))
                         .and(() -> game.state.eq(state))
                         .and(() -> game.sport.id.in(sportIds))
                         .build()
-                );
+                ).limit(pageRequestDto.size());
 
         if (state.equals(GameState.FINISHED)) {
             return gameJPAQuery
-                    .orderBy(game.startTime.desc(), game.id.asc())
+                    .orderBy(game.startTime.stringValue().concat(game.id.stringValue()).desc())
                     .fetch();
         }
 
         return gameJPAQuery
-                .orderBy(game.startTime.asc(), game.id.asc())
+                .orderBy(game.startTime.stringValue().concat(game.id.stringValue()).asc())
                 .fetch();
+    }
+
+    private String getCursorValue(Long cursor) {
+        LocalDateTime lastStartTime = findLastStartTime(cursor);
+        if (lastStartTime == null) {
+            return null;
+        }
+        return lastStartTime.format(FORMATTER) + cursor;
     }
 
     private LocalDateTime findLastStartTime(final Long cursor) {
--- src/test/java/com/sports/server/game/acceptance/GameAcceptanceTest.java
+++ src/test/java/com/sports/server/game/acceptance/GameAcceptanceTest.java
@@ -1,20 +1,20 @@
 package com.sports.server.game.acceptance;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
-
 import com.sports.server.game.dto.response.GameDetailResponse;
 import com.sports.server.game.dto.response.GameResponseDto;
 import com.sports.server.support.AcceptanceTest;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
-import java.time.LocalDateTime;
-import java.util.List;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @Sql(scripts = "/game-fixture.sql")
 class GameAcceptanceTest extends AcceptanceTest {
@@ -63,7 +63,6 @@ class GameAcceptanceTest extends AcceptanceTest {
         );
     }
 
-    @Disabled
     @Test
     void 리그의_경기를_전체_조회한다() {
 
