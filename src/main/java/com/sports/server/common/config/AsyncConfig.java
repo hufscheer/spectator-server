e43d19547c5f5d511d74dcbed73f396b77662610
does <ldk980130@gmail.com>
2023-12-24 17:44:11 +0900

    [FEAT] MDC 사용하여 correlation id 세팅

--- src/main/java/com/sports/server/common/config/AsyncConfig.java
+++ src/main/java/com/sports/server/common/config/AsyncConfig.java
@@ -1,5 +1,6 @@
 package com.sports.server.common.config;
 
+import com.sports.server.common.log.MDCTaskDecorator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -17,12 +18,13 @@ public class AsyncConfig {
     private static final String CUSTOM_THREAD_NAME_PREFIX = "ASYNC_THREAD-";
 
     @Bean(name = "asyncThreadPool")
-    public Executor asyncThreadPool() {
+    public Executor asyncThreadPool(MDCTaskDecorator mdcTaskDecorator) {
         ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
         taskExecutor.setCorePoolSize(CORE_POOL_SIZE);
         taskExecutor.setMaxPoolSize(MAX_POOL_SIZE);
         taskExecutor.setQueueCapacity(QUEUE_CAPACITY);
         taskExecutor.setThreadNamePrefix(CUSTOM_THREAD_NAME_PREFIX);
+        taskExecutor.setTaskDecorator(mdcTaskDecorator);
         taskExecutor.initialize();
         return taskExecutor;
     }
---
+++ src/main/java/com/sports/server/common/log/LogFilter.java
@@ -0,0 +1,23 @@
+package com.sports.server.common.log;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE + 1)
+@Slf4j
+public class LogFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        log.info("REQUEST URI : {}", req.getRequestURI());
+        chain.doFilter(request, response);
+    }
+}
