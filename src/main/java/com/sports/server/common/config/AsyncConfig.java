c8f9204f79349e862d6d865601a4917275db224e
does <ldk980130@gmail.com>
2023-12-25 14:06:05 +0900

    [REFACTOR] MDCDecorator를 삭제하고 내부 메서드로 변경 #90

--- src/main/java/com/sports/server/common/config/AsyncConfig.java
+++ src/main/java/com/sports/server/common/config/AsyncConfig.java
@@ -1,11 +1,13 @@
 package com.sports.server.common.config;
 
-import com.sports.server.common.log.MDCTaskDecorator;
+import org.slf4j.MDC;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskDecorator;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+import java.util.Map;
 import java.util.concurrent.Executor;
 
 @Configuration
@@ -16,15 +18,22 @@ public class AsyncConfig {
     private static final int MAX_POOL_SIZE = 10;
     private static final int QUEUE_CAPACITY = 10;
     private static final String CUSTOM_THREAD_NAME_PREFIX = "ASYNC_THREAD-";
+    private static final TaskDecorator MDC_TASK_DECORATOR = runnable -> {
+        Map<String, String> contextMap = MDC.getCopyOfContextMap();
+        return () -> {
+            MDC.setContextMap(contextMap);
+            runnable.run();
+        };
+    };
 
     @Bean(name = "asyncThreadPool")
-    public Executor asyncThreadPool(MDCTaskDecorator mdcTaskDecorator) {
+    public Executor asyncThreadPool() {
         ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
         taskExecutor.setCorePoolSize(CORE_POOL_SIZE);
         taskExecutor.setMaxPoolSize(MAX_POOL_SIZE);
         taskExecutor.setQueueCapacity(QUEUE_CAPACITY);
         taskExecutor.setThreadNamePrefix(CUSTOM_THREAD_NAME_PREFIX);
-        taskExecutor.setTaskDecorator(mdcTaskDecorator);
+        taskExecutor.setTaskDecorator(MDC_TASK_DECORATOR);
         taskExecutor.initialize();
         return taskExecutor;
     }
