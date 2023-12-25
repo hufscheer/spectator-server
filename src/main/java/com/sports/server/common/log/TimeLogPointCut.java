a1a9dbdf464a1dcc6390d2a27a35c67a52deb336
does <ldk980130@gmail.com>
2023-12-25 21:14:03 +0900

    [REFACTOR] Component 어노테이션도 로그를 찍을 수 있도록 수정 #90

--- src/main/java/com/sports/server/common/log/TimeLogPointCut.java
+++ src/main/java/com/sports/server/common/log/TimeLogPointCut.java
@@ -4,15 +4,22 @@ import org.aspectj.lang.annotation.Pointcut;
 
 public class TimeLogPointCut {
 
-    @Pointcut("packagePointCut() && ( servicePointCut() || repositoryPointCut() )")
+    @Pointcut("( commandPackagePointCut() || queryPackagePointCut()) " +
+            "&& ( componentPointCut() || servicePointCut() || repositoryPointCut() )")
     public void timeLogPointCut() {}
 
-    @Pointcut("execution(* com.sports.server..*.*(..))")
-    private void packagePointCut() {}
+    @Pointcut("execution(* com.sports.server.command..*.*(..))")
+    private void commandPackagePointCut() {}
+
+    @Pointcut("execution(* com.sports.server.query..*.*(..))")
+    private void queryPackagePointCut() {}
 
     @Pointcut("@target(org.springframework.stereotype.Service)")
     private void servicePointCut() {}
 
     @Pointcut("@target(org.springframework.stereotype.Repository)")
     private void repositoryPointCut() {}
+
+    @Pointcut("@target(org.springframework.stereotype.Component)")
+    private void componentPointCut() {}
 }
