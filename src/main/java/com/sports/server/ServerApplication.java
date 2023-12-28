df6ac714f01d4a69793cf24189711aadedd0c3e2
does <ldk980130@gmail.com>
2023-12-28 15:48:43 +0900

    [CHORE] 타임존 재설정 및 배포 브랜치 변경 #90

--- src/main/java/com/sports/server/ServerApplication.java
+++ src/main/java/com/sports/server/ServerApplication.java
@@ -1,20 +1,12 @@
 package com.sports.server;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.TimeZone;
 
 @SpringBootApplication
 public class ServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
-	}
-
-	@PostConstruct
-	void started() {
-		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 }
