bc349a3c34c0ad98cc70146697f9fea8531b369d
does <ldk980130@gmail.com>
2023-12-28 15:11:23 +0900

    [CHORE] 타임존 재설정 #90

--- src/main/java/com/sports/server/ServerApplication.java
+++ src/main/java/com/sports/server/ServerApplication.java
@@ -1,7 +1,10 @@
 package com.sports.server;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class ServerApplication {
@@ -10,4 +13,8 @@ public class ServerApplication {
 		SpringApplication.run(ServerApplication.class, args);
 	}
 
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 }
