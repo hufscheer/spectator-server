8310407e320bc3a642499f6171fd305597de9dc1
does <ldk980130@gmail.com>
2024-01-19 20:32:23 +0900

    [TEST] api 문서화 테스트 환경 구성 #94

---
+++ src/test/java/com/sports/server/support/DocumentationTest.java
@@ -0,0 +1,62 @@
+package com.sports.server.support;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sports.server.command.cheertalk.application.CheerTalkService;
+import com.sports.server.command.cheertalk.presentation.CheerTalkController;
+import com.sports.server.command.game.presentation.GameController;
+import com.sports.server.command.report.presentation.ReportController;
+import com.sports.server.common.log.TimeLogTemplate;
+import com.sports.server.query.presentation.CheerTalkQueryController;
+import com.sports.server.query.presentation.GameQueryController;
+import com.sports.server.query.presentation.LeagueQueryController;
+import com.sports.server.query.presentation.TimelineQueryController;
+import com.sports.server.support.config.RestDocsConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.restdocs.operation.preprocess.OperationRequestPreprocessor;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+
+@WebMvcTest(controllers = {
+        CheerTalkController.class,
+//        GameController.class,
+//        ReportController.class,
+//        CheerTalkQueryController.class,
+//        GameQueryController.class,
+//        LeagueQueryController.class,
+//        TimelineQueryController.class
+})
+@Import({
+        TimeLogTemplate.class,
+        RestDocsConfig.class
+})
+@AutoConfigureRestDocs
+public class DocumentationTest {
+
+    private static final OperationRequestPreprocessor HOST_INFO = preprocessRequest(modifyUris()
+            .scheme("https")
+            .host("www.api.hufstreaming.site")
+            .removePort(), prettyPrint()
+    );
+
+    protected static final RestDocumentationResultHandler RESULT_HANDLER = document(
+            "{class-name}/{method-name}",
+            HOST_INFO,
+            preprocessResponse(prettyPrint())
+    );
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @MockBean
+    protected CheerTalkService cheerTalkService;
+}
