9bb39eb52f0cb9d9971bc3f273ce31a1acbd93b8
Jin409 <sualng123@gmail.com>
2024-02-08 19:49:32 +0000

    [ADD] 엔티티 수정에 따른 flyway 마이그레이션 sql 문 수정 #103

---
+++ src/main/resources/db/migration/V6__update-timeline-lineup.sql
@@ -0,0 +1,33 @@
+ALTER TABLE records
+    RENAME COLUMN scored_quarter_id TO recorded_quarter_id;
+
+ALTER TABLE records
+    RENAME COLUMN scored_at TO recorded_at;
+
+ALTER TABLE records
+    ADD COLUMN record_type VARCHAR(255);
+
+CREATE TABLE score_records
+(
+    id               BIGINT AUTO_INCREMENT NOT NULL,
+    record_id        BIGINT                NULL,
+    lineup_player_id BIGINT                NULL,
+    score            INT                   NOT NULL
+);
+
+CREATE TABLE replacement_records
+(
+    id                        BIGINT AUTO_INCREMENT NOT NULL,
+    record_id                 BIGINT                NULL,
+    origin_lineup_player_id   BIGINT                NULL,
+    replaced_lineup_player_id BIGINT                NULL
+);
+
+ALTER TABLE league_team_players
+    ADD COLUMN number INT;
+
+ALTER TABLE lineup_players
+    ADD COLUMN number INT;
+
+ALTER TABLE lineup_players
+    ADD COLUMN is_captain BOOLEAN;
