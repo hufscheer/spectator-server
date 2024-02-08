bef8fef881c6b8434a8e1b4e8203f190f7ec1b1d
Jin409 <sualng123@gmail.com>
2024-02-08 20:53:53 +0000

    [ADD] 외래키 제약 추가 #103

--- src/main/resources/db/migration/V6__update-timeline-lineup.sql
+++ src/main/resources/db/migration/V6__update-timeline-lineup.sql
@@ -12,19 +12,36 @@ CREATE TABLE score_records
     id               BIGINT AUTO_INCREMENT NOT NULL,
     record_id        BIGINT                NOT NULL,
     lineup_player_id BIGINT                NOT NULL,
-    score            INT                   NOT NULL
+    score            INT                   NOT NULL,
+    PRIMARY KEY (id)
 );
+
+ALTER TABLE score_records
+    ADD CONSTRAINT FK_RECORDS_IN_SCORE_RECORDS FOREIGN KEY (record_id) REFERENCES records (id);
+
+ALTER TABLE score_records
+    ADD CONSTRAINT FK_LINEUP_PLAYER_ID FOREIGN KEY (lineup_player_id) REFERENCES lineup_players (id);
 
 CREATE TABLE replacement_records
 (
     id                        BIGINT AUTO_INCREMENT NOT NULL,
     record_id                 BIGINT                NOT NULL,
     origin_lineup_player_id   BIGINT                NOT NULL,
-    replaced_lineup_player_id BIGINT                NOT NULL
+    replaced_lineup_player_id BIGINT                NOT NULL,
+    PRIMARY KEY (id)
 );
 
+ALTER TABLE replacement_records
+    ADD CONSTRAINT FK_RECORDS_IN_REPLACEMENT_RECORDS FOREIGN KEY (record_id) REFERENCES records (id);
+
+ALTER TABLE replacement_records
+    ADD CONSTRAINT FK_ORIGIN_LINEUP_PLAYER FOREIGN KEY (origin_lineup_player_id) REFERENCES lineup_players (id);
+
+ALTER TABLE replacement_records
+    ADD CONSTRAINT FK_REPLACED_LINEUP_PLAYER FOREIGN KEY (replaced_lineup_player_id) REFERENCES lineup_players (id);
+
 ALTER TABLE league_team_players
-    ADD COLUMN number INT;
+    ADD COLUMN number INT NULL;
 
 ALTER TABLE lineup_players
     ADD COLUMN number INT;
