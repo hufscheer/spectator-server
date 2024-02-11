d4d3457894e0bd1bfc640f88d1c506dafefb2521
Jin409 <sualng123@gmail.com>
2024-02-11 15:49:13 +0000

    [FIX] Repository 내부 오타 수정 #102

--- src/main/java/com/sports/server/query/repository/SportQueryRepository.java
+++ src/main/java/com/sports/server/query/repository/SportQueryRepository.java
@@ -1,10 +1,9 @@
 package com.sports.server.query.repository;
 
-import com.sports.server.command.record.domain.Record;
 import com.sports.server.command.sport.domain.Sport;
 import java.util.List;
 import org.springframework.data.repository.Repository;
 
-public interface SportQueryRepository extends Repository<Record, Long> {
+public interface SportQueryRepository extends Repository<Sport, Long> {
     List<Sport> findAll();
 }
