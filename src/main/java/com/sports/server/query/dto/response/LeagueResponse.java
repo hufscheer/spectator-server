24372d4addfcbb9944e0f906ded4a90873cff0fb
does <ldk980130@gmail.com>
2024-02-24 17:23:51 +0900

    [FEAT] api 구현에 진행 중 여부 표시 추가 #113

--- src/main/java/com/sports/server/query/dto/response/LeagueResponse.java
+++ src/main/java/com/sports/server/query/dto/response/LeagueResponse.java
@@ -1,14 +1,24 @@
 package com.sports.server.query.dto.response;
 
 import com.sports.server.command.league.domain.League;
+import com.sports.server.query.application.InProgressLeagueChecker;
+
+import java.time.LocalDate;
 
 public record LeagueResponse(
         Long leagueId,
         String name,
         Integer maxRound,
-        Integer inProgressRound
+        Integer inProgressRound,
+        Boolean isInProgress
 ) {
     public LeagueResponse(League league) {
-        this(league.getId(), league.getName(), league.getMaxRound(), league.getInProgressRound());
+        this(
+                league.getId(),
+                league.getName(),
+                league.getMaxRound(),
+                league.getInProgressRound(),
+                InProgressLeagueChecker.check(LocalDate.now(), league)
+        );
     }
 }
