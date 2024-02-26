21d4068c902bca14424c760e779afdd30c938186
Jin409 <sualng123@gmail.com>
2024-02-26 16:56:40 +0000

    [ADD] 쿼리 파라미터에 리그팀 아이디 추가 #109

--- src/main/java/com/sports/server/query/dto/request/GamesQueryRequestDto.java
+++ src/main/java/com/sports/server/query/dto/request/GamesQueryRequestDto.java
@@ -11,11 +11,13 @@ public class GamesQueryRequestDto {
     private Long leagueId;
     private String stateValue;
     private List<Long> sportIds;
+    private List<Long> leagueTeamIds;
 
-    public GamesQueryRequestDto(Long league_id, String state, List<Long> sport_id) {
+    public GamesQueryRequestDto(Long league_id, String state, List<Long> sport_id, List<Long> league_team_id) {
         this.leagueId = league_id;
         this.stateValue = state;
         this.sportIds = sport_id;
+        this.leagueTeamIds = league_team_id;
     }
 
     public String getStateValue() {
