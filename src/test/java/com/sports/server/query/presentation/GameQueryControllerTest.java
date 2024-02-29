93dee3726c54618c879f3b7446c912f462609e15
Jin409 <sualng123@gmail.com>
2024-02-29 13:53:41 +0000

    [DOCS] round, league_team_id 쿼리 파라미터 api 문서에 추가 #109

--- src/test/java/com/sports/server/query/presentation/GameQueryControllerTest.java
+++ src/test/java/com/sports/server/query/presentation/GameQueryControllerTest.java
@@ -122,6 +122,8 @@ class GameQueryControllerTest extends DocumentationTest {
                 .queryParam("state", "PLAYING")
                 .queryParam("sport_id", "1")
                 .queryParam("sport_id", "2")
+                .queryParam("round", "4")
+                .queryParam("league_team_id", "1")
                 .queryParam("cursor", String.valueOf(12))
                 .queryParam("size", String.valueOf(10))
                 .contentType(MediaType.APPLICATION_JSON)
@@ -135,7 +137,9 @@ class GameQueryControllerTest extends DocumentationTest {
                                 parameterWithName("state").description("게임의 상태"),
                                 parameterWithName("sport_id").description("게임의 종목"),
                                 parameterWithName("cursor").description("페이징 커서"),
-                                parameterWithName("size").description("페이징 사이즈")
+                                parameterWithName("size").description("페이징 사이즈"),
+                                parameterWithName("league_team_id").description("리그팀의 ID"),
+                                parameterWithName("round").description("라운드")
                         ),
                         responseFields(
                                 fieldWithPath("[].id").type(JsonFieldType.NUMBER).description("게임의 ID"),
