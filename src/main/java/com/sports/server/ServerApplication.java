ae7e4331c9ba48b5f8af535d2b66738725194708
does <ldk980130@gmail.com>
2024-03-08 13:02:07 +0900

    [FIX] 타임존을 Asia/Seoul로 변경 #123

--- src/main/java/com/sports/server/ServerApplication.java
+++ src/main/java/com/sports/server/ServerApplication.java
@@ -1,12 +1,20 @@
 package com.sports.server;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class ServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
+	}
+
+	@PostConstruct
+	void started(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 }
