8e8a829aa783ea5a34b92708c160e7e295f25671
does <ldk980130@gmail.com>
2024-03-29 21:11:20 +0900

    [FIX] customException이 500에러일 경우 로깅하도록 설정 #143

--- src/main/java/com/sports/server/common/advice/ControllerExceptionAdvice.java
+++ src/main/java/com/sports/server/common/advice/ControllerExceptionAdvice.java
@@ -5,6 +5,7 @@ import com.sports.server.common.exception.CustomException;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -19,6 +20,9 @@ public class ControllerExceptionAdvice {
 
     @ExceptionHandler(CustomException.class)
     protected ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
+        if (e.getStatus().equals(HttpStatus.INTERNAL_SERVER_ERROR)) {
+            log.error(e.getMessage());
+        }
         return ResponseEntity.status(e.getStatus())
                 .body(ErrorResponse.of(e.getMessage()));
     }
