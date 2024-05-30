3ee8e1b4049b2e6e0c5753acd46e3615eccc6a0c
Jin409 <sualng123@gmail.com>
2024-05-31 07:09:49 +0900

    [ADD] 비밀번호 해싱을 위한 빈 추가 #145

---
+++ src/main/java/com/sports/server/common/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.sports.server.common.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
