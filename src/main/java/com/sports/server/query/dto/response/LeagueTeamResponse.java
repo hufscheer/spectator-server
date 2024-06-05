35c5ea6f86afe83b57ffb6e370ec207d42df80cb
fingersdanny <kbry12@naver.com>
2024-06-05 11:40:12 +0900

    refactor : LeagueTeamResponse에 logoImageUrl 포함하도록 수정

--- src/main/java/com/sports/server/query/dto/response/LeagueTeamResponse.java
+++ src/main/java/com/sports/server/query/dto/response/LeagueTeamResponse.java
@@ -4,11 +4,12 @@ import com.sports.server.command.leagueteam.LeagueTeam;
 
 public record LeagueTeamResponse(
         Long leagueTeamId,
-        String teamName
+        String teamName,
+        String logoImageUrl
 ) {
     public LeagueTeamResponse(final LeagueTeam leagueTeam) {
         this(
-                leagueTeam.getId(), leagueTeam.getName()
+                leagueTeam.getId(), leagueTeam.getName(), leagueTeam.getLogoImageUrl()
         );
     }
 }
