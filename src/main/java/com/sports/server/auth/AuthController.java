3ffcc2907191589c6c2ac5b4d31960c5b3687610
Jin409 <sualng123@gmail.com>
2024-06-09 23:25:20 +0900

    [FIX] ResponseCookie 로 변경 #145

--- src/main/java/com/sports/server/auth/AuthController.java
+++ src/main/java/com/sports/server/auth/AuthController.java
@@ -1,9 +1,9 @@
 package com.sports.server.auth;
 
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseCookie;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,8 +23,7 @@ public class AuthController {
     @PostMapping("/login")
     public void login(@RequestBody LoginVO loginVO, HttpServletResponse response) {
         JwtResponse jwtResponse = authService.login(loginVO);
-        Cookie cookie = CookieUtil.createCookie(jwtResponse.accessToken(), COOKIE_VALID_TIME);
+        ResponseCookie cookie = CookieUtil.createCookie(jwtResponse.accessToken(), COOKIE_VALID_TIME);
         response.addHeader(COOKIE_NAME, cookie.toString());
-
     }
 }
--- src/main/java/com/sports/server/auth/CookieUtil.java
+++ src/main/java/com/sports/server/auth/CookieUtil.java
@@ -1,16 +1,13 @@
 package com.sports.server.auth;
 
-import jakarta.servlet.http.Cookie;
+import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
 
 @Component
 public final class CookieUtil {
 
-    public static Cookie createCookie(final String token, final Long cookieValidTime) {
-        Cookie cookie = new Cookie(JwtTokenProvider.ACCESS_TOKEN_HEADER_STRING, token);
-        cookie.setMaxAge(Math.toIntExact(cookieValidTime));
-        cookie.setHttpOnly(true);
-        cookie.setPath("/");
-        return cookie;
+    public static ResponseCookie createCookie(final String token, final Long cookieValidTime) {
+        return ResponseCookie.from(JwtTokenProvider.ACCESS_TOKEN_HEADER_STRING, token).path("/").sameSite("Strict")
+                .secure(true).maxAge(Math.toIntExact(cookieValidTime)).httpOnly(true).build();
     }
 }
