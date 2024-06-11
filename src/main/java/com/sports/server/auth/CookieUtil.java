0fcd197d79e859f1cbe2c23766e1e59bee7d2402
Jin409 <sualng123@gmail.com>
2024-06-11 22:35:06 +0900

    [FEAT] 쿠키의 이름으로 쿠키를 꺼내오는 기능 구현 #145

--- src/main/java/com/sports/server/auth/CookieUtil.java
+++ src/main/java/com/sports/server/auth/CookieUtil.java
@@ -1,13 +1,30 @@
 package com.sports.server.auth;
 
+import com.sports.server.common.exception.UnauthorizedException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Optional;
 import org.springframework.http.ResponseCookie;
-import org.springframework.stereotype.Component;
 
-@Component
 public final class CookieUtil {
 
     public static ResponseCookie createCookie(final String token, final Long cookieValidTime) {
         return ResponseCookie.from(JwtTokenProvider.ACCESS_TOKEN_HEADER_STRING, token).path("/").sameSite("Strict")
                 .secure(true).maxAge(Math.toIntExact(cookieValidTime)).httpOnly(true).build();
+    }
+
+    public static Optional<Cookie> getCookie(HttpServletRequest request, String nameOfCookie) {
+        Cookie[] cookies = request.getCookies();
+
+        if (cookies == null || cookies.length == 0) {
+            throw new UnauthorizedException("제공된 토큰이 유효하지 않습니다. 다시 인증해주세요.");
+        }
+
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals(nameOfCookie)) {
+                return Optional.of(cookie);
+            }
+        }
+        throw new UnauthorizedException("제공된 토큰이 유효하지 않습니다. 다시 인증해주세요.");
     }
 }
