c99e2591c95391654677c935268c69f81361939d
Jin409 <sualng123@gmail.com>
2024-06-12 00:16:33 +0900

    [FEAT] 쿠키를 이용해 인가가 이루어지도록 수정 #145

--- src/main/java/com/sports/server/auth/JwtAuthorizationFilter.java
+++ src/main/java/com/sports/server/auth/JwtAuthorizationFilter.java
@@ -2,6 +2,7 @@ package com.sports.server.auth;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -18,16 +19,16 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
             throws IOException, ServletException {
 
-        String header = request.getHeader(JwtTokenProvider.ACCESS_TOKEN_HEADER_STRING);
-        if (header == null || !header.startsWith(JwtTokenProvider.TOKEN_PREFIX)) {
+        Cookie cookie = CookieUtil.getCookie(request, "TEMP").orElse(null);
+
+        if (cookie == null) {
             chain.doFilter(request, response);
             return;
         }
 
-        Authentication authentication = jwtTokenProvider.getAuthentication(header);
+        Authentication authentication = jwtTokenProvider.getAuthentication(cookie.getValue());
         SecurityContextHolder.getContext().setAuthentication(authentication);
         chain.doFilter(request, response);
     }
-
 }
 
