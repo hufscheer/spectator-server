66ea5d63cfc85f481a5c170fc468464e6ac95f94
does <ldk980130@gmail.com>
2024-06-14 23:14:22 +0900

    [FEAT] 각 라운드의 리그팀 조회 쿼리 구현 #149

--- src/main/java/com/sports/server/query/repository/LeagueTeamQueryRepository.java
+++ src/main/java/com/sports/server/query/repository/LeagueTeamQueryRepository.java
@@ -12,5 +12,10 @@ public interface LeagueTeamQueryRepository extends Repository<LeagueTeam, Long> 
     @Query("select lt from LeagueTeam lt where lt.league.id = :leagueId order by lt.name asc")
     List<LeagueTeam> findByLeagueId(@Param("leagueId") final Long leagueId);
 
-
+    @Query("select lt from LeagueTeam lt " +
+            "join GameTeam gt on lt = gt.leagueTeam " +
+            "join Game g on gt.game = g " +
+            "where lt.league.id = :leagueId and g.round = :round " +
+            "order by lt.name asc")
+    List<LeagueTeam> findByLeagueIdAndRound(@Param("leagueId") final Long leagueId, @Param("round") Integer round);
 }
