3a7fb071e098f93a9a108bc47191d061c7a0f7c3
Jin409 <sualng123@gmail.com>
2024-06-19 06:46:41 +0900

    [REFACTOR] 쿠키를 저장하는 헤더의 key 변경 #145

--- src/main/java/com/sports/server/auth/AuthController.java
+++ src/main/java/com/sports/server/auth/AuthController.java
@@ -5,6 +5,7 @@ import com.sports.server.auth.utils.CookieUtil;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,7 +26,7 @@ public class AuthController {
     @PostMapping("/manager/login")
     public void managerLogin(@RequestBody LoginVO loginVO, HttpServletResponse response) {
         JwtResponse jwtResponse = authService.managerLogin(loginVO);
-        ResponseCookie cookie = CookieUtil.createCookie(jwtResponse.accessToken(), COOKIE_VALID_TIME);
-        response.addHeader(COOKIE_NAME, cookie.toString());
+        ResponseCookie cookie = CookieUtil.createCookie(COOKIE_NAME, jwtResponse.accessToken(), COOKIE_VALID_TIME);
+        response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
     }
 }
