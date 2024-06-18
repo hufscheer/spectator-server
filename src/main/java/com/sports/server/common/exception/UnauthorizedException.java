210118d00cf9539dbb97fbc4a51716e3e36c278b
Jin409 <sualng123@gmail.com>
2024-06-18 22:20:43 +0900

    [FIX] CustomException 으로 message 를 전달하지 않는 오류 수정 #145

--- src/main/java/com/sports/server/common/exception/UnauthorizedException.java
+++ src/main/java/com/sports/server/common/exception/UnauthorizedException.java
@@ -4,6 +4,6 @@ import org.springframework.http.HttpStatus;
 
 public class UnauthorizedException extends CustomException {
     public UnauthorizedException(String message) {
-        super(HttpStatus.UNAUTHORIZED);
+        super(HttpStatus.UNAUTHORIZED, message);
     }
 }
