d534bf6bf95e30a03b9146f90dd66ba86bb23036
does <ldk980130@gmail.com>
2024-06-20 22:48:43 +0900

    [FEAT] Timeline 엔티티 작성 #154

---
+++ src/main/java/com/sports/server/command/timeline/domain/Timeline.java
@@ -0,0 +1,34 @@
+package com.sports.server.command.timeline.domain;
+
+import com.sports.server.command.game.domain.Game;
+import com.sports.server.command.game.domain.GameTeam;
+import com.sports.server.command.sport.domain.Quarter;
+import com.sports.server.common.domain.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "timelines")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "type", discriminatorType = DiscriminatorType.STRING)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public abstract class Timeline extends BaseEntity<Timeline> {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "game_id")
+    private Game game;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "game_team_id")
+    private GameTeam gameTeam;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "recorded_quarter_id")
+    private Quarter recordedQuarter;
+
+    @Column(name = "recorded_at", nullable = false)
+    private Integer recordedAt;
+}
