68a74319f2a768586bfe34eb402d81f520ac15d3
does <ldk980130@gmail.com>
2024-06-20 23:36:27 +0900

    [REFACTOR] PKTimeline 엔티티 작성 #154

---
+++ src/main/java/com/sports/server/command/timeline/domain/PKTimeline.java
@@ -0,0 +1,18 @@
+package com.sports.server.command.timeline.domain;
+
+import com.sports.server.command.game.domain.LineupPlayer;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@DiscriminatorValue("PK")
+@Getter
+public class PKTimeline extends Timeline {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "scorer_id")
+    private LineupPlayer scorer;
+
+    @Column(name = "is_success")
+    private Boolean isSuccess;
+}
