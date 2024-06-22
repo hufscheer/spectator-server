fe312216971d3a8cfc8ebdce7a011ac232c3e00f
Jin409 <sualng123@gmail.com>
2024-06-23 01:12:27 +0900

    [REFACTOR] 쿠키가 존재하지 않는 경우 null 이 아닌 예외를 던지도록 수정 #145

--- src/main/java/com/sports/server/auth/filter/JwtAuthorizationFilter.java
+++ src/main/java/com/sports/server/auth/filter/JwtAuthorizationFilter.java
@@ -1,10 +1,11 @@
 package com.sports.server.auth.filter;
 
+import com.sports.server.auth.exception.AuthorizationErrorMessages;
 import com.sports.server.auth.jwt.JwtProvider;
 import com.sports.server.auth.utils.CookieUtil;
+import com.sports.server.common.exception.UnauthorizedException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -24,22 +25,22 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
 
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
             throws IOException, ServletException {
+        CookieUtil.getCookie(request, COOKIE_NAME)
+                .ifPresentOrElse(
+                        cookie -> {
+                            Authentication authentication = jwtProvider.getAuthentication(cookie.getValue());
+                            SecurityContextHolder.getContext().setAuthentication(authentication);
+                        },
+                        () -> {
+                            throw new UnauthorizedException(AuthorizationErrorMessages.PERMISSION_DENIED);
+                        }
+                );
 
-        Cookie cookie = CookieUtil.getCookie(request, COOKIE_NAME).orElse(null);
-
-        if (cookie == null) {
-            chain.doFilter(request, response);
-            return;
-        }
-
-        Authentication authentication = jwtProvider.getAuthentication(
-                cookie.getValue());
-        SecurityContextHolder.getContext().setAuthentication(authentication);
         chain.doFilter(request, response);
     }
 
     @Override
-    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+    protected boolean shouldNotFilter(HttpServletRequest request) {
         String path = request.getRequestURI();
         if (path.contains("/manager/login")) {
             return true;
