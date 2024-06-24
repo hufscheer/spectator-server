c462050649850d1d99b3e4461023345e2f489ee0
Jin409 <sualng123@gmail.com>
2024-06-25 08:52:15 +0900

    [TEST] 로그인 성공 시 테스트 추가 #145

--- src/test/java/com/sports/server/auth/application/AuthServiceTest.java
+++ src/test/java/com/sports/server/auth/application/AuthServiceTest.java
@@ -1,12 +1,18 @@
 package com.sports.server.auth.application;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+import com.sports.server.auth.dto.JwtResponse;
 import com.sports.server.auth.dto.LoginRequest;
 import com.sports.server.auth.exception.AuthorizationErrorMessages;
+import com.sports.server.auth.utils.JwtUtil;
 import com.sports.server.common.exception.NotFoundException;
 import com.sports.server.common.exception.UnauthorizedException;
 import com.sports.server.support.ServiceTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -18,54 +24,91 @@ public class AuthServiceTest extends ServiceTest {
     @Autowired
     private AuthService authService;
 
-    @Test
-    void 존재하지_않는_사용자로_로그인을_시도할_때_예외가_발생한다() {
-        // given
-        String email = "notexist@example.com";
-        String password = "1234";
-        LoginRequest loginRequest = new LoginRequest(email, password);
+    @Autowired
+    private JwtUtil jwtUtil;
 
-        // when & then
-        assertThatThrownBy(
-                () -> authService.loginByManager(loginRequest)
-        ).isInstanceOf(NotFoundException.class)
-                .hasMessage(AuthorizationErrorMessages.MEMBER_NOT_FOUND_EXCEPTION)
-                .extracting("status").isEqualTo(HttpStatus.NOT_FOUND);
+    @Nested
+    @DisplayName("로그인을 할 때 성공하는 경우")
+    class LoginSuccessTest {
+        @Test
+        void 예외를_던지지_않는다() {
+            // given
+            String email = "john@example.com";
+            String password = "1234";
+            LoginRequest loginRequest = new LoginRequest(email, password);
 
+            // when & then
+            assertDoesNotThrow(() -> {
+                authService.loginByManager(loginRequest);
+            });
+        }
+
+        @Test
+        void 올바른_액세스_토큰을_반환한다() {
+            // given
+            String email = "john@example.com";
+            String password = "1234";
+            LoginRequest loginRequest = new LoginRequest(email, password);
+
+            // when & then
+            JwtResponse response = authService.loginByManager(loginRequest);
+            String accessToken = response.accessToken();
+            assertEquals(email, jwtUtil.getEmail(accessToken));
+        }
     }
 
-    @Test
-    void 잘못된_비밀번호로_로그인을_시도할_때_예외가_발샐한다() {
-        // given
-        String email = "john@example.com";
-        String password = "notvalidpassword";
-        LoginRequest loginRequest = new LoginRequest(email, password);
+    @Nested
+    @DisplayName("로그인을 할 때")
+    class LoginFailureTest {
 
-        // when & then
-        assertThatThrownBy(
-                () -> authService.loginByManager(loginRequest)
-        ).isInstanceOf(NotFoundException.class)
-                .hasMessage(AuthorizationErrorMessages.MEMBER_NOT_FOUND_EXCEPTION)
-                .extracting("status").isEqualTo(HttpStatus.NOT_FOUND);
+        @Test
+        void 존재하지_않는_사용자인_경우_예외가_발생한다() {
+            // given
+            String email = "notexist@example.com";
+            String password = "1234";
+            LoginRequest loginRequest = new LoginRequest(email, password);
 
-    }
+            // when & then
+            assertThatThrownBy(
+                    () -> authService.loginByManager(loginRequest)
+            ).isInstanceOf(NotFoundException.class)
+                    .hasMessage(AuthorizationErrorMessages.MEMBER_NOT_FOUND_EXCEPTION)
+                    .extracting("status").isEqualTo(HttpStatus.NOT_FOUND);
 
+        }
 
-    @Test
-    void 매니저가_아닌_경우_로그인을_시도할_때_예외가_발생한다() {
+        @Test
+        void 잘못된_비밀번호인_경우_예외가_발샐한다() {
+            // given
+            String email = "john@example.com";
+            String password = "invalid-password";
+            LoginRequest loginRequest = new LoginRequest(email, password);
 
-        // given
-        String email = "jane@example.com";
-        String password = "1234";
-        LoginRequest loginRequest = new LoginRequest(email, password);
+            // when & then
+            assertThatThrownBy(
+                    () -> authService.loginByManager(loginRequest)
+            ).isInstanceOf(NotFoundException.class)
+                    .hasMessage(AuthorizationErrorMessages.MEMBER_NOT_FOUND_EXCEPTION)
+                    .extracting("status").isEqualTo(HttpStatus.NOT_FOUND);
 
-        // when & then
-        assertThatThrownBy(
-                () -> authService.loginByManager(loginRequest)
-        ).isInstanceOf(UnauthorizedException.class)
-                .hasMessage(AuthorizationErrorMessages.PERMISSION_DENIED)
-                .extracting("status").isEqualTo(HttpStatus.UNAUTHORIZED);
+        }
 
+        @Test
+        void 매니저가_권한이_없는_경우_예외가_발생한다() {
+
+            // given
+            String email = "jane@example.com";
+            String password = "1234";
+            LoginRequest loginRequest = new LoginRequest(email, password);
+
+            // when & then
+            assertThatThrownBy(
+                    () -> authService.loginByManager(loginRequest)
+            ).isInstanceOf(UnauthorizedException.class)
+                    .hasMessage(AuthorizationErrorMessages.PERMISSION_DENIED)
+                    .extracting("status").isEqualTo(HttpStatus.UNAUTHORIZED);
+
+        }
     }
 
 }
