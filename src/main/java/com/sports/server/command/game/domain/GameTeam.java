eb792eeee9090af86ae680ced5137a64495f1fd3
fingersdanny <kbry12@naver.com>
2024-06-26 01:47:16 +0900

    refactor : GameTeam 변경 - GameTeam에 LineupPlayer에 대한 양방향 의존성 추가 - GameTeam에 각각 선발을 후보로, 후보를 선발로 돌리는 메서드 추가

--- src/main/java/com/sports/server/command/game/domain/GameTeam.java
+++ src/main/java/com/sports/server/command/game/domain/GameTeam.java
@@ -1,17 +1,24 @@
 package com.sports.server.command.game.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 import com.sports.server.command.leagueteam.LeagueTeam;
 import com.sports.server.common.domain.BaseEntity;
 import com.sports.server.common.exception.CustomException;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
 import org.springframework.http.HttpStatus;
 
 @Entity
@@ -20,37 +27,63 @@ import org.springframework.http.HttpStatus;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class GameTeam extends BaseEntity<GameTeam> {
 
-    private static final int MAXIMUM_OF_CHEER_COUNT = 500;
-    private static final int MAXIMUM_OF_TOTAL_CHEER_COUNT = 100_000_000;
-    private static final int MINIMUM_OF_CHEER_COUNT = 0;
+	private static final int MAXIMUM_OF_CHEER_COUNT = 500;
+	private static final int MAXIMUM_OF_TOTAL_CHEER_COUNT = 100_000_000;
+	private static final int MINIMUM_OF_CHEER_COUNT = 0;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "game_id")
-    private Game game;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "game_id")
+	private Game game;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "league_team_id")
-    private LeagueTeam leagueTeam;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "league_team_id")
+	private LeagueTeam leagueTeam;
 
-    @Column(name = "cheer_count", nullable = false)
-    private int cheerCount;
+	@OneToMany(mappedBy = "gameTeam")
+	private List<LineupPlayer> lineupPlayers = new ArrayList<>();
 
-    @Column(name = "score", nullable = false)
-    private int score;
+	@Column(name = "cheer_count", nullable = false)
+	private int cheerCount;
 
-    public void validateCheerCountOfGameTeam(final int cheerCount) {
-        if (cheerCount >= MAXIMUM_OF_CHEER_COUNT || cheerCount <= MINIMUM_OF_CHEER_COUNT) {
-            throw new CustomException(HttpStatus.BAD_REQUEST, "잘못된 범위의 응원 요청 횟수입니다.");
-        }
-        if (this.cheerCount + cheerCount > MAXIMUM_OF_TOTAL_CHEER_COUNT) {
-            throw new CustomException(HttpStatus.BAD_REQUEST, "총 응원 횟수가 한계에 도달했습니다.");
-        }
-    }
+	@Column(name = "score", nullable = false)
+	private int score;
 
-    public boolean matchGame(final Game game) {
-        return this.game.equals(game);
-    }
+	public void validateCheerCountOfGameTeam(final int cheerCount) {
+		if (cheerCount >= MAXIMUM_OF_CHEER_COUNT || cheerCount <= MINIMUM_OF_CHEER_COUNT) {
+			throw new CustomException(HttpStatus.BAD_REQUEST, "잘못된 범위의 응원 요청 횟수입니다.");
+		}
+		if (this.cheerCount + cheerCount > MAXIMUM_OF_TOTAL_CHEER_COUNT) {
+			throw new CustomException(HttpStatus.BAD_REQUEST, "총 응원 횟수가 한계에 도달했습니다.");
+		}
+	}
 
+	public boolean matchGame(final Game game) {
+		return this.game.equals(game);
+	}
+
+	public void registerStarter(final LineupPlayer lineupPlayer) {
+		this.lineupPlayers.stream()
+			.filter(lp -> {
+				if (Objects.equals(lp, lineupPlayer) && Objects.equals(lp.getState(), LineupPlayerState.STARTER)) {
+					throw new CustomException(HttpStatus.BAD_REQUEST, "이미 선발로 등록된 선수입니다.");
+				}
+				return Objects.equals(lp, lineupPlayer);
+			})
+			.findAny()
+			.ifPresent(LineupPlayer::changeStateToStarter);
+	}
+
+	public void rollBackToCandidate(final LineupPlayer lineupPlayer) {
+		this.lineupPlayers.stream()
+			.filter(lp -> {
+				if (Objects.equals(lp, lineupPlayer) && Objects.equals(lp.getState(), LineupPlayerState.CANDIDATE)) {
+					throw new CustomException(HttpStatus.BAD_REQUEST, "이미 후보로 내려간 선수입니다.");
+				}
+				return Objects.equals(lp, lineupPlayer);
+			})
+			.findAny()
+			.ifPresent(LineupPlayer::changeStateToCandidate);
+	}
 }
 
 
