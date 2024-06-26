6ab1dc86dc91f373ca3c43f69d4ec82575ead922
fingersdanny <kbry12@naver.com>
2024-06-26 12:08:29 +0900

    refactor : LineupPlayerService 라인업 선수 상태 변경 메서드 분리

--- src/main/java/com/sports/server/command/game/application/LineupPlayerService.java
+++ src/main/java/com/sports/server/command/game/application/LineupPlayerService.java
@@ -3,9 +3,9 @@ package com.sports.server.command.game.application;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.sports.server.command.game.domain.LineupPlayerRepository;
-import com.sports.server.command.game.domain.LineupPlayerState;
-import com.sports.server.command.game.dto.LineupPlayerStateUpdateRequest;
+import com.sports.server.command.game.domain.Game;
+import com.sports.server.command.game.domain.LineupPlayer;
+import com.sports.server.common.application.EntityUtils;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,9 +13,17 @@ import lombok.RequiredArgsConstructor;
 @Transactional
 @RequiredArgsConstructor
 public class LineupPlayerService {
-	private final LineupPlayerRepository lineupPlayerRepository;
+	private final EntityUtils entityUtils;
 
-	public void updatePlayerState(final LineupPlayerStateUpdateRequest request) {
-		lineupPlayerRepository.updatePlayerState(LineupPlayerState.from(request.state()), request.lineupPlayerIds());
+	public void changePlayerStateToStarter(final Long gameId, final Long lineupPlayerId) {
+		Game game = entityUtils.getEntity(gameId, Game.class);
+		LineupPlayer lineupPlayer = entityUtils.getEntity(lineupPlayerId, LineupPlayer.class);
+		game.registerStarter(lineupPlayer);
+	}
+
+	public void changePlayerStateToCandidate(final Long gameId, final Long lineupPlayerId) {
+		Game game = entityUtils.getEntity(gameId, Game.class);
+		LineupPlayer lineupPlayer = entityUtils.getEntity(lineupPlayerId, LineupPlayer.class);
+		game.rollbackToCandidate(lineupPlayer);
 	}
 }
