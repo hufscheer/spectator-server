76624cd051aab0dbc7687918dc4e58dec6d35d10
does <ldk980130@gmail.com>
2024-06-27 23:01:05 +0900

    [REFACTOR] TimelineQueryService 리팩터링 #156

--- src/main/java/com/sports/server/query/application/timeline/TimelineQueryService.java
+++ src/main/java/com/sports/server/query/application/timeline/TimelineQueryService.java
@@ -1,8 +1,10 @@
 package com.sports.server.query.application.timeline;
 
 import com.sports.server.command.sport.domain.Quarter;
+import com.sports.server.command.timeline.domain.Timeline;
 import com.sports.server.query.dto.response.RecordResponse;
 import com.sports.server.query.dto.response.TimelineResponse;
+import com.sports.server.query.repository.TimelineQueryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,6 +22,7 @@ import static java.util.stream.Collectors.groupingBy;
 public class TimelineQueryService {
 
     private final List<RecordQueryService> recordQueryServices;
+    private final TimelineQueryRepository timelineQueryRepository;
 
     public List<TimelineResponse> getTimeline(final Long gameId) {
         Map<Quarter, List<RecordResponse>> records = getRecordsGroupByQuarter(gameId);
@@ -38,5 +41,19 @@ public class TimelineQueryService {
                 .flatMap(recordQueryService -> recordQueryService.findByGameId(gameId).stream())
                 .sorted(comparingInt(RecordResponse::recordedAt).reversed())
                 .collect(groupingBy(RecordResponse::quarter));
+    }
+
+    public List<TimelineResponse> getTimelines(final Long gameId) {
+        Map<Quarter, List<Timeline>> timelines = timelineQueryRepository.findByGameId(gameId)
+                .stream()
+                .collect(groupingBy(Timeline::getRecordedQuarter));
+
+        return timelines.keySet()
+                .stream()
+                .sorted(comparingLong(Quarter::getId).reversed())
+                .map(quarter -> TimelineResponse.of(
+                        quarter.getName(),
+                        timelines.get(quarter)
+                )).toList();
     }
 }
--- src/main/java/com/sports/server/query/dto/response/TimelineResponse.java
+++ src/main/java/com/sports/server/query/dto/response/TimelineResponse.java
@@ -1,9 +1,14 @@
 package com.sports.server.query.dto.response;
 
+import com.sports.server.command.timeline.domain.Timeline;
+
 import java.util.List;
 
 public record TimelineResponse(
         String gameQuarter,
         List<RecordResponse> records
 ) {
+    public static TimelineResponse of(String quarter, List<Timeline> timelines) {
+        return null;
+    }
 }
