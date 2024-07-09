72d3f51854f734fbaa57208c91b51d9f9581ba35
Jin409 <sualng123@gmail.com>
2024-07-10 02:54:09 +0900

    [TEST] 리그팀 등록 테스트 추가 #157

--- src/main/java/com/sports/server/command/leagueteam/domain/LeagueTeamRepository.java
+++ src/main/java/com/sports/server/command/leagueteam/domain/LeagueTeamRepository.java
@@ -1,7 +1,11 @@
 package com.sports.server.command.leagueteam.domain;
 
+import com.sports.server.command.league.domain.League;
+import java.util.Optional;
 import org.springframework.data.repository.Repository;
 
 public interface LeagueTeamRepository extends Repository<LeagueTeam, Long> {
     void save(LeagueTeam leagueTeam);
+
+    Optional<LeagueTeam> findByLeagueAndName(League league, String name);
 }
