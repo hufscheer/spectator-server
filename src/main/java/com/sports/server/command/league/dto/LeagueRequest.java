0d04496535aecc57c884053ca7287795d1e58a91
fingersdanny <kbry12@naver.com>
2024-07-12 12:48:13 +0900

    refactor : LeagueRequest에 organizationId 추가

--- src/main/java/com/sports/server/command/league/dto/LeagueRequest.java
+++ src/main/java/com/sports/server/command/league/dto/LeagueRequest.java
@@ -3,9 +3,10 @@ package com.sports.server.command.league.dto;
 import java.time.LocalDateTime;
 
 public record LeagueRequest(
-	String name,
 	LocalDateTime startAt,
 	LocalDateTime endAt,
-	Integer maxRound
+	Integer maxRound,
+	Long organizationId,
+	String name
 ) {
 }
