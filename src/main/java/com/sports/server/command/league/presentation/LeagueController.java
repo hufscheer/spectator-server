7a5334a2e500b7e8117fd14b93864c62faa1f2cb
fingersdanny <kbry12@naver.com>
2024-07-12 12:48:43 +0900

    feat : LeagueController 추가

---
+++ src/main/java/com/sports/server/command/league/presentation/LeagueController.java
@@ -0,0 +1,26 @@
+package com.sports.server.command.league.presentation;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sports.server.command.league.application.LeagueService;
+import com.sports.server.command.league.dto.LeagueRequest;
+import com.sports.server.command.member.domain.Member;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/manager")
+public class LeagueController {
+	private final LeagueService leagueService;
+
+	@PostMapping("/leagues")
+	public ResponseEntity<Void> register(@RequestBody final LeagueRequest leagueRequest, Member member) {
+		leagueService.register(member, leagueRequest);
+		return ResponseEntity.ok().build();
+	}
+}
