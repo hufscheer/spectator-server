abd16803d851a91e7580e0d85c1621cd38f9db1e
Jin409 <sualng123@gmail.com>
2024-07-17 00:39:08 +0900

    [TEST] 리그팀 수정 인수 테스트 추가 #163

--- src/test/java/com/sports/server/command/leagueteam/acceptance/LeagueTeamAcceptanceTest.java
+++ src/test/java/com/sports/server/command/leagueteam/acceptance/LeagueTeamAcceptanceTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import com.sports.server.command.leagueteam.dto.LeagueTeamRegisterRequest;
 import com.sports.server.command.leagueteam.dto.LeagueTeamRegisterRequest.LeagueTeamPlayerRegisterRequest;
+import com.sports.server.command.leagueteam.dto.LeagueTeamUpdateRequest;
 import com.sports.server.support.AcceptanceTest;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
@@ -37,6 +38,34 @@ public class LeagueTeamAcceptanceTest extends AcceptanceTest {
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .body(request)
                 .post("/leagues/{leagueId}/teams")
+                .then().log().all()
+                .extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    @Test
+    void 리그팀을_수정한다() {
+        // given
+        Long leagueId = 1L;
+        Long teamId = 3L;
+        List<LeagueTeamUpdateRequest.LeagueTeamPlayerRegisterRequest> playerRegisterRequests = List.of(
+                new LeagueTeamUpdateRequest.LeagueTeamPlayerRegisterRequest("name-a", 1),
+                new LeagueTeamUpdateRequest.LeagueTeamPlayerRegisterRequest("name-b", 2));
+        LeagueTeamUpdateRequest request = new LeagueTeamUpdateRequest(
+                "name", "logo-image-url", playerRegisterRequests, List.of(1L, 2L));
+
+        configureMockJwtForEmail("john.doe@example.com");
+
+        // when
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .cookie(COOKIE_NAME, mockToken)
+                .pathParam("leagueId", leagueId)
+                .pathParam("teamId", teamId)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(request)
+                .put("/leagues/{leagueId}/teams/{teamId}")
                 .then().log().all()
                 .extract();
 
