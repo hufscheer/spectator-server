c2af89f8f59981a2891f172bc74422eb1feb0c23
fingersdanny <kbry12@naver.com>
2024-07-17 07:31:30 +0900

    test : 리그 생성에 대한 문서화 테스트 추가

--- src/test/java/com/sports/server/command/league/presentation/LeagueControllerTest.java
+++ src/test/java/com/sports/server/command/league/presentation/LeagueControllerTest.java
@@ -1,5 +1,7 @@
 package com.sports.server.command.league.presentation;
 
+import static org.mockito.Mockito.*;
+import static org.springframework.restdocs.cookies.CookieDocumentation.*;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -12,7 +14,10 @@ import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.ResultActions;
 
 import com.sports.server.command.league.dto.LeagueRequest;
+import com.sports.server.command.member.domain.Member;
 import com.sports.server.support.DocumentationTest;
+
+import jakarta.servlet.http.Cookie;
 
 class LeagueControllerTest extends DocumentationTest {
 
@@ -20,11 +25,15 @@ class LeagueControllerTest extends DocumentationTest {
 	void 리그를_생성한다() throws Exception {
 		// given
 		LeagueRequest request = new LeagueRequest(1L, "우물정 제기차기 대회", 4, LocalDateTime.now(), LocalDateTime.now());
+		Cookie cookie = new Cookie(COOKIE_NAME, "temp-cookie");
+
+		doNothing().when(leagueService).register(any(Member.class), any(LeagueRequest.class));
 
 		// when
-		ResultActions result = mockMvc.perform(post("/manager/leagues", request)
+		ResultActions result = mockMvc.perform(post("/leagues", request)
 			.contentType(MediaType.APPLICATION_JSON)
-			.content(objectMapper.writeValueAsString(request)));
+			.content(objectMapper.writeValueAsString(request))
+			.cookie(cookie));
 
 		// then
 		result.andExpect(status().isOk())
@@ -35,6 +44,9 @@ class LeagueControllerTest extends DocumentationTest {
 						fieldWithPath("maxRound").type(JsonFieldType.NUMBER).description("대회 진행 라운드 수"),
 						fieldWithPath("startAt").type(JsonFieldType.STRING).description("대회 시작 시간"),
 						fieldWithPath("endAt").type(JsonFieldType.STRING).description("대회 종료 시간")
+					),
+					requestCookies(
+						cookieWithName(COOKIE_NAME).description("로그인을 통해 얻은 토큰")
 					)
 				)
 			);
