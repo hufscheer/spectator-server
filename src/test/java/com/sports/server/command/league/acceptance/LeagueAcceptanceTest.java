2baf545ddd35f9ba3dd71d5a3197cecd11bddc17
fingersdanny <kbry12@naver.com>
2024-07-17 07:33:52 +0900

    refactor : 대회 생성 인수테스트 경로 수정

--- src/test/java/com/sports/server/command/league/acceptance/LeagueAcceptanceTest.java
+++ src/test/java/com/sports/server/command/league/acceptance/LeagueAcceptanceTest.java
@@ -7,6 +7,7 @@ import java.time.LocalDateTime;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
 
 import com.sports.server.command.league.dto.LeagueRequest;
 import com.sports.server.support.AcceptanceTest;
@@ -15,18 +16,21 @@ import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 
-class LeagueAcceptanceTest extends AcceptanceTest {
+@Sql("/league-fixture.sql")
+public class LeagueAcceptanceTest extends AcceptanceTest {
 	@Test
 	void 대회를_저장한다() throws Exception {
 		// given
 		LeagueRequest request = new LeagueRequest(1L, "우물정 제기차기 대회", 4, LocalDateTime.now(), LocalDateTime.now());
 
+		configureMockJwtForEmail("john.doe@example.com");
+
 		// when
 		ExtractableResponse<Response> response = RestAssured.given().log().all()
-			.when()
+			.cookie(COOKIE_NAME, mockToken)
 			.contentType(MediaType.APPLICATION_JSON_VALUE)
 			.body(request)
-			.post("/manager/leagues")
+			.post("/leagues")
 			.then().log().all()
 			.extract();
 
