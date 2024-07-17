3b908c62a88c54cdd64aa78eff6cd318b457657a
fingersdanny <kbry12@naver.com>
2024-07-17 21:05:07 +0900

    refactor : FixtureMonkeyUtils에 LeagueTest에 사용할 arbitrary 메서드 추가

--- src/test/java/com/sports/server/support/fixture/FixtureMonkeyUtils.java
+++ src/test/java/com/sports/server/support/fixture/FixtureMonkeyUtils.java
@@ -1,13 +1,17 @@
 package com.sports.server.support.fixture;
 
+import java.util.Random;
+
+import net.jqwik.api.Arbitraries;
+import net.jqwik.api.Arbitrary;
+import net.jqwik.api.arbitraries.StringArbitrary;
+
 import com.navercorp.fixturemonkey.ArbitraryBuilder;
 import com.navercorp.fixturemonkey.FixtureMonkey;
 import com.navercorp.fixturemonkey.api.introspector.FieldReflectionArbitraryIntrospector;
 
-import java.util.Random;
-
 public class FixtureMonkeyUtils {
-    private static final FixtureMonkey INSTANCE = FixtureMonkey.builder()
+    public static final FixtureMonkey INSTANCE = FixtureMonkey.builder()
             .objectIntrospector(FieldReflectionArbitraryIntrospector.INSTANCE)
             .build();
 
@@ -16,5 +20,13 @@ public class FixtureMonkeyUtils {
     public static <T> ArbitraryBuilder<T> entityBuilder(Class<T> clazz) {
         return INSTANCE.giveMeBuilder(clazz)
                 .set("id", RANDOM.nextLong(1, 10000));
+    }
+
+    public static Arbitrary<Integer> maxRoundArbitrary() {
+        return Arbitraries.of(4, 8, 16, 32);
+    }
+
+    public static StringArbitrary nameArbitrary() {
+        return Arbitraries.strings();
     }
 }
