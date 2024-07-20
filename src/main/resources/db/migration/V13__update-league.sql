99122ca3852de0a999f460394fc6ab464d5edf6f
fingersdanny <kbry12@naver.com>
2024-07-20 12:55:55 +0900

    feat : flyway sql v13 추가
    - 기존 라운드를 숫자 타입에서 VARCHAR(255)로 변경 하고 기존 값들을 변환

---
+++ src/main/resources/db/migration/V13__update-league.sql
@@ -0,0 +1,21 @@
+ALTER TABLE leagues
+    MODIFY COLUMN max_round VARCHAR(255),
+    MODIFY COLUMN in_progress_round VARCHAR(255);
+
+UPDATE leagues
+    SET max_round = CASE
+        WHEN max_round = '2' THEN '결승'
+        WHEN max_round = '4' THEN '4강'
+        WHEN max_round = '8' THEN '8강'
+        WHEN max_round = '16' THEN '16강'
+        WHEN max_round = '32' THEN '32강'
+        ELSE max_round
+    END,
+    in_progress_round = CASE
+        WHEN in_progress_round = '2' THEN '결승'
+        WHEN in_progress_round = '4' THEN '4강'
+        WHEN in_progress_round = '8' THEN '8강'
+        WHEN in_progress_round = '16' THEN '16강'
+        WHEN in_progress_round = '32' THEN '32강'
+        ELSE in_progress_round
+    END;
