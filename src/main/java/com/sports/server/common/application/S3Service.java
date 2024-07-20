0df5ab4df56417710241236c4feca9cc4c41030b
Jin409 <sualng123@gmail.com>
2024-07-20 23:05:47 +0100

    [RENAME] S3Service 로 클래스명 변경 #163

--- src/main/java/com/sports/server/common/application/S3Service.java
+++ src/main/java/com/sports/server/common/application/S3Service.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class PresignedUrlGenerator {
+public class S3Service {
 
     private final AmazonS3 amazonS3;
 
