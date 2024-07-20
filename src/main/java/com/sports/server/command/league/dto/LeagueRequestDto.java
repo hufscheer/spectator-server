82d525e59727edf3ce989e7288c0e1e540552b00
fingersdanny <kbry12@naver.com>
2024-07-20 12:47:32 +0900

    refactor : 정해진 컨벤션에 따라 LeagueDto 에서 LeagueRequestDto로 이름 변경

--- src/main/java/com/sports/server/command/league/dto/LeagueRequestDto.java
+++ src/main/java/com/sports/server/command/league/dto/LeagueRequestDto.java
@@ -3,19 +3,20 @@ package com.sports.server.command.league.dto;
 import java.time.LocalDateTime;
 
 import com.sports.server.command.league.domain.League;
+import com.sports.server.command.league.domain.LeagueRound;
 import com.sports.server.command.member.domain.Member;
 import com.sports.server.command.organization.domain.Organization;
 
-public class LeagueDto {
-	public record RegisterRequest(
+public class LeagueRequestDto {
+	public record Register(
 		Long organizationId,
 		String name,
-		Integer maxRound,
+		String maxRound,
 		LocalDateTime startAt,
 		LocalDateTime endAt
 	) {
 		public League toEntity(final Member manager, final Organization organization) {
-			return new League(manager, organization, name, startAt, endAt, maxRound);
+			return new League(manager, organization, name, startAt, endAt, LeagueRound.from(maxRound));
 		}
 	}
 }
