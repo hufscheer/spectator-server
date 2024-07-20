8b538072de0adb7cb531baa9ff428280fc1791b9
fingersdanny <kbry12@naver.com>
2024-07-20 12:54:12 +0900

    refactor : FixtureMonkeyUtils의 maxRound arbitrary 수정

--- src/test/java/com/sports/server/support/fixture/FixtureMonkeyUtils.java
+++ src/test/java/com/sports/server/support/fixture/FixtureMonkeyUtils.java
@@ -6,6 +6,8 @@ import net.jqwik.api.Arbitraries;
 import net.jqwik.api.Arbitrary;
 import net.jqwik.api.arbitraries.StringArbitrary;
 
+import com.sports.server.command.league.domain.LeagueRound;
+
 import com.navercorp.fixturemonkey.ArbitraryBuilder;
 import com.navercorp.fixturemonkey.FixtureMonkey;
 import com.navercorp.fixturemonkey.api.introspector.FieldReflectionArbitraryIntrospector;
@@ -22,8 +24,8 @@ public class FixtureMonkeyUtils {
                 .set("id", RANDOM.nextLong(1, 10000));
     }
 
-    public static Arbitrary<Integer> maxRoundArbitrary() {
-        return Arbitraries.of(4, 8, 16, 32);
+    public static Arbitrary<LeagueRound> maxRoundArbitrary() {
+        return Arbitraries.of(LeagueRound.class);
     }
 
     public static StringArbitrary nameArbitrary() {
