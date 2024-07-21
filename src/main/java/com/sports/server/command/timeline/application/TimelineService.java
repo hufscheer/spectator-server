cba49b891a2f640fe61be67889b216354cd36d8c
does <ldk980130@gmail.com>
2024-07-21 18:57:24 +0900

    [FEAT] 교체 타임라인 생성 서비스 로직 설계 #161

--- src/main/java/com/sports/server/command/timeline/application/TimelineService.java
+++ src/main/java/com/sports/server/command/timeline/application/TimelineService.java
@@ -5,6 +5,7 @@ import com.sports.server.command.game.domain.LineupPlayer;
 import com.sports.server.command.member.domain.Member;
 import com.sports.server.command.sport.domain.Quarter;
 import com.sports.server.command.timeline.TimelineDto;
+import com.sports.server.command.timeline.domain.ReplacementTimeline;
 import com.sports.server.command.timeline.domain.ScoreTimeline;
 import com.sports.server.command.timeline.domain.TimelineRepository;
 import com.sports.server.common.application.EntityUtils;
@@ -21,26 +22,43 @@ public class TimelineService {
     private final EntityUtils entityUtils;
 
     public void registerScore(Member member, Long gameId, TimelineDto.RegisterScore request) {
-        Game game = entityUtils.getEntity(gameId, Game.class);
-
-        if (!game.isMangedBy(member)) {
-            throw new UnauthorizedException("타임라인 생성 권한이 없습니다.");
-        }
-
-        Quarter quarter = entityUtils.getEntity(request.recordedQuarterId(), Quarter.class);
-        LineupPlayer scorer = entityUtils.getEntity(request.scoreLineupPlayerId(), LineupPlayer.class);
-
         ScoreTimeline timeline = ScoreTimeline.score(
-                game,
-                quarter,
+                checkPermissionAndGet(gameId, member),
+                getQuarter(request.recordedQuarterId()),
                 request.recordedAt(),
-                scorer
+                getPlayer(request.scoreLineupPlayerId())
         );
 
         timelineRepository.save(timeline);
     }
 
     public void registerReplacement(Member member, Long gameId, TimelineDto.RegisterReplacement request) {
+        ReplacementTimeline timeline = new ReplacementTimeline(
+                checkPermissionAndGet(gameId, member),
+                getQuarter(request.recordedQuarterId()),
+                request.recordedAt(),
+                getPlayer(request.originLineupPlayerId()),
+                getPlayer(request.replacementLineupPlayerId())
+        );
 
+        timelineRepository.save(timeline);
+    }
+
+    private Game checkPermissionAndGet(Long gameId, Member member) {
+        Game game = entityUtils.getEntity(gameId, Game.class);
+
+        if (!game.isMangedBy(member)) {
+            throw new UnauthorizedException("타임라인 생성 권한이 없습니다.");
+        }
+
+        return game;
+    }
+
+    private Quarter getQuarter(Long quarterId) {
+        return entityUtils.getEntity(quarterId, Quarter.class);
+    }
+
+    private LineupPlayer getPlayer(Long playerId) {
+        return entityUtils.getEntity(playerId, LineupPlayer.class);
     }
 }
