bfbc67ac301deb7db58a02eddcf331e7219fe03b
fingersdanny <kbry12@naver.com>
2024-08-07 12:36:53 +0900

    [FEAT] 대회 정보 수정용 메서드 추가

--- src/main/java/com/sports/server/command/league/application/LeagueService.java
+++ src/main/java/com/sports/server/command/league/application/LeagueService.java
@@ -3,7 +3,9 @@ package com.sports.server.command.league.application;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.sports.server.command.league.domain.League;
 import com.sports.server.command.league.domain.LeagueRepository;
+import com.sports.server.command.league.domain.Round;
 import com.sports.server.command.league.dto.LeagueRequestDto;
 import com.sports.server.command.member.domain.Member;
 import com.sports.server.command.organization.domain.Organization;
@@ -21,5 +23,10 @@ public class LeagueService {
 	public void register(final Member manager, final LeagueRequestDto.Register request) {
 		Organization organization = entityUtils.getEntity(request.organizationId(), Organization.class);
 		leagueRepository.save(request.toEntity(manager, organization));
+	}
+
+	public void update(final Member manager, final LeagueRequestDto.Update request, final Long leagueId) {
+		League league = entityUtils.getEntity(leagueId, League.class);
+		league.updateInfo(request.name(), request.startAt(), request.endAt(), Round.from(request.maxRound()));
 	}
 }
