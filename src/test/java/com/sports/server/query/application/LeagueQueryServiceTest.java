2bdba2c6b814038d77de47cc1c3baab987945e18
Jin409 <sualng123@gmail.com>
2024-08-09 17:13:18 +0900

    [TEST] 서비스 테스트 추가 #192

--- src/test/java/com/sports/server/query/application/LeagueQueryServiceTest.java
+++ src/test/java/com/sports/server/query/application/LeagueQueryServiceTest.java
@@ -2,10 +2,20 @@ package com.sports.server.query.application;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
+import com.sports.server.command.member.domain.Member;
+import com.sports.server.common.application.EntityUtils;
+import com.sports.server.query.dto.response.LeagueResponseForManager;
+import com.sports.server.query.dto.response.LeagueResponseForManager.GameDetailResponse;
+import com.sports.server.query.dto.response.LeagueResponseForManager.GameDetailResponse.GameTeamResponse;
 import com.sports.server.query.dto.response.LeagueTeamResponse;
 import com.sports.server.support.ServiceTest;
 import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.jdbc.Sql;
@@ -15,6 +25,9 @@ public class LeagueQueryServiceTest extends ServiceTest {
 
     @Autowired
     LeagueQueryService leagueQueryService;
+
+    @Autowired
+    EntityUtils entityUtils;
 
     @Test
     void 리그에_해당하는_리그팀을_모두_조회한다() {
@@ -32,6 +45,94 @@ public class LeagueQueryServiceTest extends ServiceTest {
                 () -> assertThat(leagueTeam.sizeOfLeagueTeamPlayers()).isEqualTo(4)
         );
 
+    }
+
+    @Nested
+    @DisplayName("매니저가 생성한 리그만을 조회할 때")
+    class LeaguesByManagerTest {
+
+        private Member manager;
+        private List<LeagueResponseForManager> response;
+
+        @BeforeEach
+        void setUp() {
+            manager = entityUtils.getEntity(1L, Member.class);
+            response = leagueQueryService.findLeaguesByManager(manager);
+        }
+
+        private Optional<LeagueResponseForManager> getFirstLeagueById(Long leagueId) {
+            return response.stream()
+                    .filter(league -> league.id().equals(leagueId))
+                    .findFirst();
+        }
+
+        @Test
+        void 다른_매니저가_생성한_리그는_조회되지_않는다() {
+            // then
+            List<Long> ids = response.stream()
+                    .map(LeagueResponseForManager::id)
+                    .toList();
+            assertFalse(ids.contains(8L), "다른 매니저가 생성한 리그는 조회 되어서는 안됩니다.");
+        }
+
+        @Test
+        void 진행중이_아닌_경기가_반환되어서는_안된다() {
+            // given
+            Long leagueId = 1L;
+            Long nonPlayingGameId = 2L;
+
+            // then
+            LeagueResponseForManager firstLeague = getFirstLeagueById(leagueId)
+                    .orElseThrow(() -> new AssertionError("리그가 존재하지 않습니다."));
+
+            List<Long> ids = firstLeague.inProgressGames()
+                    .stream()
+                    .map(GameDetailResponse::id)
+                    .toList();
+
+            assertFalse(ids.contains(nonPlayingGameId), "진행 중이 아닌 경기가 반환되어서는 안됩니다.");
+        }
+
+        @Test
+        void 다른_리그에_속한_경기가_반환되어서는_안된다() {
+            // given
+            Long leagueId = 1L;
+            Long otherLeagueGameId = 4L;
+
+            // then
+            LeagueResponseForManager firstLeague = getFirstLeagueById(leagueId)
+                    .orElseThrow(() -> new AssertionError("리그가 존재하지 않습니다."));
+
+            List<Long> ids = firstLeague.inProgressGames()
+                    .stream()
+                    .map(GameDetailResponse::id)
+                    .toList();
+
+            assertFalse(ids.contains(otherLeagueGameId), "다른 리그에 속한 경기가 반환되어서는 안됩니다.");
+        }
+
+        @Test
+        void 경기에_알맞는_게임팀이_반환된다() {
+            // given
+            Long leagueId = 1L;
+            Long gameId = 1L;
+
+            // then
+            LeagueResponseForManager firstLeague = getFirstLeagueById(leagueId)
+                    .orElseThrow(() -> new AssertionError("리그가 존재하지 않습니다."));
+
+            List<Long> idsOfGameTeamsOfFirstGame = firstLeague.inProgressGames()
+                    .stream()
+                    .filter(game -> game.id().equals(gameId))
+                    .findFirst()
+                    .orElseThrow(() -> new AssertionError("게임이 존재하지 않습니다."))
+                    .gameTeams()
+                    .stream()
+                    .map(GameTeamResponse::gameTeamId)
+                    .toList();
+
+            assertThat(idsOfGameTeamsOfFirstGame).isEqualTo(List.of(1L, 2L));
+        }
     }
 
 }
