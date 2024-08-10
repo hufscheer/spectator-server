930f5a3707f0c01be4355a8b0812e48cb8a8b03a
Jin409 <sualng123@gmail.com>
2024-08-10 18:03:41 +0900

    [DEL] 사용되지 않는 어노테이션 삭제 #183

--- src/main/java/com/sports/server/command/game/presentation/GameController.java
+++ src/main/java/com/sports/server/command/game/presentation/GameController.java
@@ -14,12 +14,10 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping
 public class GameController {
 
     private final GameTeamService gameTeamService;
