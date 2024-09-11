4bb45524b79130ee9425e6dcabeb29da358f5a61
Zena0128 <0128ja@gmail.com>
2024-09-11 17:40:45 +0900

    [DOCS] 경기 수정 기능 문서화 추가

--- src/test/java/com/sports/server/command/game/presentation/GameControllerTest.java
+++ src/test/java/com/sports/server/command/game/presentation/GameControllerTest.java
@@ -1,6 +1,8 @@
 package com.sports.server.command.game.presentation;
 
-
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.doNothing;
 import static org.springframework.restdocs.cookies.CookieDocumentation.cookieWithName;
 import static org.springframework.restdocs.cookies.CookieDocumentation.requestCookies;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.patch;
@@ -9,10 +11,12 @@ import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWit
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.sports.server.command.game.dto.CheerCountUpdateRequest;
 import com.sports.server.command.game.dto.GameRequestDto;
+import com.sports.server.command.member.domain.Member;
 import com.sports.server.support.DocumentationTest;
 import jakarta.servlet.http.Cookie;
 import java.time.LocalDateTime;
@@ -131,6 +135,42 @@ public class GameControllerTest extends DocumentationTest {
                                         .description("경기게 참여하는 첫번째 리그팀의 아이디"),
                                 fieldWithPath("idOfTeam2").type(JsonFieldType.NUMBER)
                                         .description("경기게 참여하는 두번째 리그팀의 아이디"),
+                                fieldWithPath("videoId").type(JsonFieldType.STRING)
+                                        .description("경기 영상 링크")
+                        ),
+                        requestCookies(
+                                cookieWithName(COOKIE_NAME).description("로그인을 통해 얻은 토큰")
+                        )
+                ));
+    }
+
+    @Test
+    void 경기를_수정한다() throws Exception {
+
+        // given
+        Long leagueId = 1L;
+        Long gameId = 1L;
+        GameRequestDto.Update requestDto = new GameRequestDto.Update(
+                "게임 이름", "16강", "전반전", "PLAYING", LocalDateTime.now(), "videoId"
+        );
+
+        doNothing().when(gameService).updateGame(anyLong(), anyLong(), any(GameRequestDto.Update.class), any(Member.class));
+
+        // when
+        ResultActions result = mockMvc.perform(put("/leagues/{leagueId}/{gameId}", leagueId, gameId)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(requestDto))
+                .cookie(new Cookie(COOKIE_NAME, "temp-cookie")));
+
+        // then
+        result.andExpect(status().isOk())
+                .andDo(restDocsHandler.document(
+                        requestFields(
+                                fieldWithPath("name").type(JsonFieldType.STRING).description("변경할 경기의 이름"),
+                                fieldWithPath("round").type(JsonFieldType.STRING).description("변경할 경기의 라운드 ex. 16강, 결승"),
+                                fieldWithPath("quarter").type(JsonFieldType.STRING).description("쿼터"),
+                                fieldWithPath("state").type(JsonFieldType.STRING).description("경기의 상태"),
+                                fieldWithPath("startTime").type(JsonFieldType.STRING).description("시작 날짜 및 시각"),
                                 fieldWithPath("videoId").type(JsonFieldType.STRING)
                                         .description("경기 영상 링크")
                         ),
