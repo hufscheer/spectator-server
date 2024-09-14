dcfb8e04e3c3ab9a05d5f5c2d339d61f68d077ac
Jin409 <sualng123@gmail.com>
2024-09-14 16:15:47 +0900

    [TEST] 서비스 테스트 추가 #215

--- src/test/java/com/sports/server/query/application/LineupPlayerQueryServiceTest.java
+++ src/test/java/com/sports/server/query/application/LineupPlayerQueryServiceTest.java
@@ -2,8 +2,11 @@ package com.sports.server.query.application;
 
 import static org.junit.Assert.assertEquals;
 
+import com.sports.server.command.game.domain.LineupPlayer;
 import com.sports.server.query.dto.response.LineupPlayerResponse;
+import com.sports.server.query.dto.response.LineupPlayerResponse.PlayerResponse;
 import com.sports.server.support.ServiceTest;
+import com.sports.server.support.fixture.LineupPlayerFixtureRepository;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,6 +19,9 @@ public class LineupPlayerQueryServiceTest extends ServiceTest {
 
     @Autowired
     private LineupPlayerQueryService lineupPlayerQueryService;
+
+    @Autowired
+    private LineupPlayerFixtureRepository lineupPlayerFixtureRepository;
 
     @Test
     void 라인업_조회시_게임팀_아이디의_오름차순으로_반환된다() {
@@ -36,5 +42,21 @@ public class LineupPlayerQueryServiceTest extends ServiceTest {
                 gameTeamIds, sortedGameTeamIds
         );
 
+    }
+
+    @Test
+    void 출전_선수_조회시_출전상태인_선수들만_조회된다() {
+
+        // given
+        Long gameId = 1L;
+        List<LineupPlayer> playingPlayers = lineupPlayerFixtureRepository.findPlayingPlayersByGameId(gameId);
+
+        // when
+        List<LineupPlayerResponse> responses = lineupPlayerQueryService.getPlayingLineup(gameId);
+
+        // then
+        assertEquals(playingPlayers.stream().map(LineupPlayer::getId).toList(), responses.stream()
+                .flatMap(lp -> lp.gameTeamPlayers().stream().map(PlayerResponse::id))
+                .collect(Collectors.toList()));
     }
 }
