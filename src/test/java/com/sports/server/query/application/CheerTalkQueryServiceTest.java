131a0777ce97c9a6d66647a3da2637ffc84a36ae
Jin409 <sualng123@gmail.com>
2024-09-22 16:43:38 +0900

    [TEST] 서비스 테스트 추가 #218

--- src/test/java/com/sports/server/query/application/CheerTalkQueryServiceTest.java
+++ src/test/java/com/sports/server/query/application/CheerTalkQueryServiceTest.java
@@ -11,6 +11,7 @@ import com.sports.server.common.dto.PageRequestDto;
 import com.sports.server.common.exception.UnauthorizedException;
 import com.sports.server.query.dto.response.CheerTalkResponse;
 import com.sports.server.support.ServiceTest;
+import java.util.Comparator;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -28,21 +29,21 @@ public class CheerTalkQueryServiceTest extends ServiceTest {
     @Autowired
     private EntityUtils entityUtils;
 
+    private PageRequestDto pageRequestDto;
+
+    private Member manager;
+
+    @BeforeEach
+    void setUp() {
+        pageRequestDto = new PageRequestDto(
+                null, 10
+        );
+        manager = entityUtils.getEntity(1L, Member.class);
+    }
+
     @Nested
     @DisplayName("신고된 응원톡을 조회할 때")
     class TestFindReportedCheerTalksByLeagueId {
-
-        private PageRequestDto pageRequestDto;
-
-        private Member manager;
-
-        @BeforeEach
-        void setUp() {
-            pageRequestDto = new PageRequestDto(
-                    null, 10
-            );
-            manager = entityUtils.getEntity(1L, Member.class);
-        }
 
         @Test
         void 신고된_응원톡만_조회된다() {
@@ -91,6 +92,65 @@ public class CheerTalkQueryServiceTest extends ServiceTest {
         }
 
 
+    }
+
+    @Nested
+    @DisplayName("블락되지 않은 리그의 응원톡을 전체 조회 할 때")
+    class TestFindUnblockedCheerTalksByLeagueId {
+
+        private Long leagueId;
+
+        @BeforeEach
+        void setUp() {
+            leagueId = 1L;
+        }
+
+        @Test
+        void 최신순으로_조회된다() {
+
+            // when
+            List<CheerTalkResponse.ForManager> results = cheerTalkQueryService.getUnblockedCheerTalksByLeagueId(
+                    leagueId, pageRequestDto, manager);
+
+            // then
+            assertAll(
+                    () -> assertThat(results)
+                            .map(CheerTalkResponse.ForManager::cheerTalkId)
+                            .containsExactly(13L, 12L, 11L, 10L, 9L, 8L, 7L, 6L, 5L, 4L),
+                    () -> assertThat(results)
+                            .map(CheerTalkResponse.ForManager::createdAt)
+                            .isSortedAccordingTo(Comparator.reverseOrder())
+            );
+        }
+
+
+        @Test
+        void 차단되지_않은_응원톡만_조회된다() {
+            // given
+            Long leagueId = 1L;
+
+            // when
+            List<CheerTalkResponse.ForManager> results = cheerTalkQueryService.getUnblockedCheerTalksByLeagueId(
+                    leagueId, pageRequestDto, manager);
+
+            // then
+            assertThat(
+                    results.stream().map(CheerTalkResponse.ForManager::isBlocked)
+            ).containsOnly(false);
+        }
+
+        @Test
+        void 해당_리그의_응원톡만_조회된다() {
+            // when
+            List<CheerTalkResponse.ForManager> responses = cheerTalkQueryService.getReportedCheerTalksByLeagueId(
+                    leagueId, pageRequestDto, manager);
+
+            // then
+            assertThat(
+                    responses.stream()
+                            .map(CheerTalkResponse.ForManager::leagueId).toList()
+            ).containsOnly(leagueId);
+        }
     }
 
 
