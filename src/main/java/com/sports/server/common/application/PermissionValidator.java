47cfc96a5dde09c4a8fda844c817cd6a758b60fc
Jin409 <sualng123@gmail.com>
2024-09-26 13:56:33 +0900

    [REFACTOR] 에러 메시지 자세하게 수정 #218

--- src/main/java/com/sports/server/common/application/PermissionValidator.java
+++ src/main/java/com/sports/server/common/application/PermissionValidator.java
@@ -1,6 +1,5 @@
 package com.sports.server.common.application;
 
-import com.sports.server.auth.exception.AuthorizationErrorMessages;
 import com.sports.server.command.member.domain.Member;
 import com.sports.server.common.domain.ManagedEntity;
 import com.sports.server.common.exception.UnauthorizedException;
@@ -9,7 +8,8 @@ public class PermissionValidator {
 
     public static <T extends ManagedEntity> void checkPermission(final T entity, final Member manager) {
         if (!entity.isManagedBy(manager)) {
-            throw new UnauthorizedException(AuthorizationErrorMessages.PERMISSION_DENIED);
+            throw new UnauthorizedException(
+                    manager.getEmail() + " 은 " + entity.getClass() + " 에 접근할 권한이 존재하지 않습니다.");
         }
     }
 }
