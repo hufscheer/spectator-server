a5d43a087fd91ed58bd9e18e64715e3f1f1df724
Jin409 <sualng123@gmail.com>
2024-09-28 16:20:01 +0900

    [DOCS] 문서화 #242

--- src/test/java/com/sports/server/query/presentation/TimelineQueryControllerTest.java
+++ src/test/java/com/sports/server/query/presentation/TimelineQueryControllerTest.java
@@ -1,22 +1,26 @@
 package com.sports.server.query.presentation;
 
-import com.sports.server.command.timeline.domain.GameProgressType;
-import com.sports.server.query.dto.response.*;
-import com.sports.server.support.DocumentationTest;
-import org.junit.jupiter.api.Test;
-import org.mockito.BDDMockito;
-import org.springframework.http.MediaType;
-import org.springframework.restdocs.payload.JsonFieldType;
-import org.springframework.test.web.servlet.ResultActions;
-
-import java.util.List;
-
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.sports.server.command.timeline.domain.GameProgressType;
+import com.sports.server.query.dto.response.PkRecordResponse;
+import com.sports.server.query.dto.response.ProgressRecordResponse;
+import com.sports.server.query.dto.response.RecordResponse;
+import com.sports.server.query.dto.response.ReplacementRecordResponse;
+import com.sports.server.query.dto.response.ScoreRecordResponse;
+import com.sports.server.query.dto.response.TimelineResponse;
+import com.sports.server.support.DocumentationTest;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.web.servlet.ResultActions;
 
 public class TimelineQueryControllerTest extends DocumentationTest {
 
@@ -40,7 +44,7 @@ public class TimelineQueryControllerTest extends DocumentationTest {
                         new TimelineResponse(
                                 QUARTER2, List.of(
                                 new RecordResponse(
-                                        null, 1L,  SCORE_TYPE,
+                                        null, 1L, SCORE_TYPE,
                                         13,
                                         "선수10",
                                         1L,
@@ -53,10 +57,11 @@ public class TimelineQueryControllerTest extends DocumentationTest {
                                                         TEAM_B, TEAM_B_IMAGE_URL, 3)
                                         )),
                                         new ReplacementRecordResponse(1L, "선수3"),
-                                        new ProgressRecordResponse(GameProgressType.QUARTER_START)
+                                        new ProgressRecordResponse(GameProgressType.QUARTER_START),
+                                        new PkRecordResponse(1L, true)
                                 ),
                                 new RecordResponse(
-                                        null, 1L,  REPLACEMENT_TYPE,
+                                        null, 1L, REPLACEMENT_TYPE,
                                         10,
                                         "선수2",
                                         1L,
@@ -69,7 +74,8 @@ public class TimelineQueryControllerTest extends DocumentationTest {
                                                         TEAM_B, TEAM_B_IMAGE_URL, 0)
                                         )),
                                         new ReplacementRecordResponse(1L, "선수3"),
-                                        new ProgressRecordResponse(GameProgressType.QUARTER_END)
+                                        new ProgressRecordResponse(GameProgressType.QUARTER_END),
+                                        new PkRecordResponse(4L, false)
                                 )
                         ))
                 ));
@@ -89,26 +95,42 @@ public class TimelineQueryControllerTest extends DocumentationTest {
                                 fieldWithPath("[].gameQuarter").type(JsonFieldType.STRING).description("쿼터의 이름"),
                                 fieldWithPath("[].records[].recordId").type(JsonFieldType.NUMBER).description("기록의 ID"),
                                 fieldWithPath("[].records[].type").type(JsonFieldType.STRING).description("기록의 타입"),
-                                fieldWithPath("[].records[].recordedAt").type(JsonFieldType.NUMBER).description("기록된 시간"),
-                                fieldWithPath("[].records[].playerName").type(JsonFieldType.STRING).description("기록의 대상 선수 이름"),
-                                fieldWithPath("[].records[].gameTeamId").type(JsonFieldType.NUMBER).description("기록의 대상 게임 팀 ID"),
-                                fieldWithPath("[].records[].teamName").type(JsonFieldType.STRING).description("기록의 대상 팀 이름"),
-                                fieldWithPath("[].records[].teamImageUrl").type(JsonFieldType.STRING).description("기록의 대상 팀 이미지"),
-                                fieldWithPath("[].records[].scoreRecord.score").type(JsonFieldType.NUMBER).description("SCORE 타입일 때 득점한 점수"),
+                                fieldWithPath("[].records[].recordedAt").type(JsonFieldType.NUMBER)
+                                        .description("기록된 시간"),
+                                fieldWithPath("[].records[].playerName").type(JsonFieldType.STRING)
+                                        .description("기록의 대상 선수 이름"),
+                                fieldWithPath("[].records[].gameTeamId").type(JsonFieldType.NUMBER)
+                                        .description("기록의 대상 게임 팀 ID"),
+                                fieldWithPath("[].records[].teamName").type(JsonFieldType.STRING)
+                                        .description("기록의 대상 팀 이름"),
+                                fieldWithPath("[].records[].teamImageUrl").type(JsonFieldType.STRING)
+                                        .description("기록의 대상 팀 이미지"),
+                                fieldWithPath("[].records[].scoreRecord.score").type(JsonFieldType.NUMBER)
+                                        .description("SCORE 타입일 때 득점한 점수"),
                                 fieldWithPath("[].records[].scoreRecord.scoreRecordId").type(JsonFieldType.NUMBER)
                                         .description("SCORE 타입 기록의 ID"),
                                 fieldWithPath("[].records[].scoreRecord.snapshot[].teamName").type(JsonFieldType.STRING)
                                         .description("SCORE 타입일 때 점수 스냅샷에 표시할 팀 이름"),
-                                fieldWithPath("[].records[].scoreRecord.snapshot[].teamImageUrl").type(JsonFieldType.STRING)
+                                fieldWithPath("[].records[].scoreRecord.snapshot[].teamImageUrl").type(
+                                                JsonFieldType.STRING)
                                         .description("SCORE 타입일 때 점수 스냅샷에 표시할 팀 이미지"),
                                 fieldWithPath("[].records[].scoreRecord.snapshot[].score").type(JsonFieldType.NUMBER)
                                         .description("SCORE 타입일 때 점수 스냅샷에 표시할 점수"),
-                                fieldWithPath("[].records[].replacementRecord.replacementRecordId").type(JsonFieldType.NUMBER)
+                                fieldWithPath("[].records[].replacementRecord.replacementRecordId").type(
+                                                JsonFieldType.NUMBER)
                                         .description("REPLACEMENT 타입 기록의  ID"),
-                                fieldWithPath("[].records[].replacementRecord.replacedPlayerName").type(JsonFieldType.STRING)
+                                fieldWithPath("[].records[].replacementRecord.replacedPlayerName").type(
+                                                JsonFieldType.STRING)
                                         .description("REPLACEMENT 타입일 때 교체되어 IN 되는 선수"),
                                 fieldWithPath("[].records[].progressRecord.gameProgressType").type(JsonFieldType.STRING)
-                                        .description("PROGRESS 타입일 때 게임 진행 상태 타입 (GAME_START, QUARTER_START, QUARTER_END, GAME_END)")
+                                        .description(
+                                                "PROGRESS 타입일 때 게임 진행 상태 타입 (GAME_START, QUARTER_START, QUARTER_END, GAME_END)"),
+                                fieldWithPath("[].records[].pkRecord.pkRecordId").type(
+                                                JsonFieldType.NUMBER)
+                                        .description("PK 타입 기록의  ID"),
+                                fieldWithPath("[].records[].pkRecord.isSuccess").type(
+                                                JsonFieldType.BOOLEAN)
+                                        .description("승부차기 득점 성공 여부")
                         )
                 ));
 
