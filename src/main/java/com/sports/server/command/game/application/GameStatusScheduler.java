7f26d4739daaca4b5280eb40624971e879bdbf3d
Jin409 <sualng123@gmail.com>
2024-11-26 15:56:36 +0900

    [FEAT] 매일 자정마다 스케줄러가 실행되도록 하는 설정 추가 #249

---
+++ src/main/java/com/sports/server/command/game/application/GameStatusScheduler.java
@@ -0,0 +1,19 @@
+package com.sports.server.command.game.application;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GameStatusScheduler {
+
+    private final GameService gameService;
+
+    public GameStatusScheduler(GameService gameService) {
+        this.gameService = gameService;
+    }
+
+    @Scheduled(cron = "0 0 0 * * *", zone = "Asia/Seoul")
+    public void scheduleUpdateGameStatusToFinish() {
+        gameService.updateGameStatusToFinish();
+    }
+}
