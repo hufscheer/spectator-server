550272e5619d73454bfef8a6b58927741bd9eb81
양효빈 <hyobin3026@naver.com>
2025-03-24 14:53:02 +0900

    [FEATURE] 선수 등록 시 학번 받기 구현

--- src/main/java/com/sports/server/ServerApplication.java
+++ src/main/java/com/sports/server/ServerApplication.java
@@ -1,16 +1,7 @@
 package com.sports.server;
 
-import com.sports.server.command.member.domain.Member;
-import com.sports.server.command.member.domain.MemberRepository;
-import jakarta.annotation.PostConstruct;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.Optional;
-import java.util.TimeZone;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableScheduling
 @SpringBootApplication
