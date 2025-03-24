b0eab91683558b4ea8ec15cc9ff74f22d149da54
Jin409 <sualng123@gmail.com>
2025-03-24 20:47:19 +0900

    [FIX] 서버 구동

--- src/main/java/com/sports/server/ServerApplication.java
+++ src/main/java/com/sports/server/ServerApplication.java
@@ -1,5 +1,6 @@
 package com.sports.server;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -8,6 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class ServerApplication {
 
     public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
     }
 
 //    @PostConstruct
