ff466b7cd65a046decf358cae82c00d0459af61e
Jin409 <sualng123@gmail.com>
2025-09-23 21:07:11 +0900

    [CHORE] 부하가 가지 않도록 추가적인 설정

--- src/main/java/com/sports/server/common/config/CloudWatchCustomConfig.java
+++ src/main/java/com/sports/server/common/config/CloudWatchCustomConfig.java
@@ -15,8 +15,9 @@ import software.amazon.awssdk.services.cloudwatch.CloudWatchAsyncClient;
 @Configuration
 public class CloudWatchCustomConfig {
 
-    private static final String CLOUDWATCH_NAMESPACE = "HUFSCHEER/prod";
-    private static final Duration CLOUDWATCH_STEP = Duration.ofMinutes(1);
+    private static final String NAMESPACE = "HUFSCHEER/prod";
+    private static final Duration STEP = Duration.ofMinutes(5);
+    private static final String BATCH_SIZE = "20";
 
     @Bean
     public CloudWatchConfig cloudWatchConfig() {
@@ -24,41 +25,35 @@ public class CloudWatchCustomConfig {
 
             @Override
             public String get(String key) {
+                if ("cloudwatch.batchSize".equals(key)) return BATCH_SIZE;
+                if ("cloudwatch.enabled".equals(key)) return "true";
                 return null;
             }
 
             @Override
             public String namespace() {
-                return CLOUDWATCH_NAMESPACE;
+                return NAMESPACE;
             }
 
             @Override
             public Duration step() {
-                return CLOUDWATCH_STEP;
+                return STEP;
             }
         };
     }
 
     @Bean
     public CloudWatchAsyncClient cloudWatchAsyncClient() {
-        return CloudWatchAsyncClient.builder()
-                .region(Region.AP_NORTHEAST_2)
-                .build();
+        return CloudWatchAsyncClient.builder().region(Region.AP_NORTHEAST_2).build();
     }
 
     @Bean
-    public CloudWatchMeterRegistry cloudWatchMeterRegistry(CloudWatchConfig config,
-                                                           CloudWatchAsyncClient client) {
+    public CloudWatchMeterRegistry cloudWatchMeterRegistry(CloudWatchConfig config, CloudWatchAsyncClient client) {
         return new CloudWatchMeterRegistry(config, Clock.SYSTEM, client);
     }
 
     @Bean
     public MeterFilter allowOnlySelected() {
-        return MeterFilter.denyUnless(id ->
-                id.getName().startsWith("jvm.") ||
-                        id.getName().startsWith("http.server.") ||
-                        id.getName().startsWith("tomcat.") ||
-                        id.getName().startsWith("hikaricp.")
-        );
+        return MeterFilter.denyUnless(id -> id.getName().startsWith("jvm.") || id.getName().startsWith("http.server.") || id.getName().startsWith("tomcat.") || id.getName().startsWith("hikaricp."));
     }
 }
