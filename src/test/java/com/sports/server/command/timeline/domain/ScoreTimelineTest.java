f3542752e5527f5b0f79c9683b12052a13aa9d31
양효빈 <hyobin3026@naver.com>
2025-09-28 16:22:33 +0900

    [FIX] 경고 타임라인 응답 버그 픽스

--- src/test/java/com/sports/server/command/timeline/domain/ScoreTimelineTest.java
+++ src/test/java/com/sports/server/command/timeline/domain/ScoreTimelineTest.java
@@ -1,67 +1,67 @@
-//package com.sports.server.command.timeline.domain;
-//
-//import com.sports.server.command.game.domain.Game;
-//import com.sports.server.command.game.domain.GameTeam;
-//import com.sports.server.command.game.domain.LineupPlayer;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Nested;
-//import org.junit.jupiter.api.Test;
-//
-//import java.util.ArrayList;
-//
-//import static com.sports.server.support.fixture.FixtureMonkeyUtils.entityBuilder;
-//import static org.assertj.core.api.Assertions.assertThat;
-//
-//class ScoreTimelineTest {
-//    private final Game game = entityBuilder(Game.class)
-//            .set("teams", new ArrayList<>())
-//            .sample();
-//
-//    private final Quarter quarter = entityBuilder(Quarter.class)
-//            .sample();
-//
-//    @BeforeEach
-//    void setUp() {
-//        game.addGameTeam(entityBuilder(GameTeam.class)
-//                .set("game", game)
-//                .set("id", 1L)
-//                .set("score", 1)
-//                .sample());
-//
-//        game.addGameTeam(entityBuilder(GameTeam.class)
-//                .set("game", game)
-//                .set("id", 2L)
-//                .set("score", 2)
-//                .sample());
-//    }
-//
-//    @Nested
-//    @DisplayName("점수 타임라인을")
-//    class CreateTest {
-//        @Test
-//        void team1_득점으로_생성한다() {
-//            // given
-//            LineupPlayer scorer = entityBuilder(LineupPlayer.class)
-//                    .set("gameTeam", game.getTeam1())
-//                    .sample();
-//
-//            // when
-//            ScoreTimeline timeline = ScoreTimeline.score(
-//                    game,
-//                    quarter,
-//                    10,
-//                    scorer
-//            );
-//
-//            timeline.apply();
-//
-//            // then
-//            assertThat(timeline.getScorer()).isEqualTo(scorer);
-//            assertThat(timeline.getSnapshotScore1()).isEqualTo(2);
-//            assertThat(timeline.getSnapshotScore2()).isEqualTo(2);
-//        }
-//
+package com.sports.server.command.timeline.domain;
+
+import com.sports.server.command.game.domain.Game;
+import com.sports.server.command.game.domain.GameTeam;
+import com.sports.server.command.game.domain.LineupPlayer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static com.sports.server.support.fixture.FixtureMonkeyUtils.entityBuilder;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ScoreTimelineTest {
+    private final Game game = entityBuilder(Game.class)
+            .set("teams", new ArrayList<>())
+            .sample();
+
+    private final Quarter quarter = entityBuilder(Quarter.class)
+            .sample();
+
+    @BeforeEach
+    void setUp() {
+        game.addGameTeam(entityBuilder(GameTeam.class)
+                .set("game", game)
+                .set("id", 1L)
+                .set("score", 1)
+                .sample());
+
+        game.addGameTeam(entityBuilder(GameTeam.class)
+                .set("game", game)
+                .set("id", 2L)
+                .set("score", 2)
+                .sample());
+    }
+
+    @Nested
+    @DisplayName("점수 타임라인을")
+    class CreateTest {
+        @Test
+        void team1_득점으로_생성한다() {
+            // given
+            LineupPlayer scorer = entityBuilder(LineupPlayer.class)
+                    .set("gameTeam", game.getTeam1())
+                    .sample();
+
+            // when
+            ScoreTimeline timeline = ScoreTimeline.score(
+                    game,
+                    quarter,
+                    10,
+                    scorer
+            );
+
+            timeline.apply();
+
+            // then
+            assertThat(timeline.getScorer()).isEqualTo(scorer);
+            assertThat(timeline.getSnapshotScore1()).isEqualTo(2);
+            assertThat(timeline.getSnapshotScore2()).isEqualTo(2);
+        }
+
 //        @Test
 //        void team2_득점으로_생성한다() {
 //            // given
@@ -84,5 +84,5 @@
 //            assertThat(timeline.getSnapshotScore1()).isEqualTo(1);
 //            assertThat(timeline.getSnapshotScore2()).isEqualTo(3);
 //        }
-//    }
-//}
+    }
+}
