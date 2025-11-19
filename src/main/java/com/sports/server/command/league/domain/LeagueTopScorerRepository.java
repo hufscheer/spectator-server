049ea943f0f6a97300793d8ee2101d1ddb2a9717
Jin409 <sualng123@gmail.com>
2025-11-19 13:42:17 +0900

    refactor: batch insert 를 통해 DB 왕복 횟수 절감

--- src/main/java/com/sports/server/command/league/domain/LeagueTopScorerRepository.java
+++ src/main/java/com/sports/server/command/league/domain/LeagueTopScorerRepository.java
@@ -10,19 +10,21 @@ import java.util.List;
 
 public interface LeagueTopScorerRepository extends Repository<LeagueTopScorer, Long> {
     LeagueTopScorer save(LeagueTopScorer leagueTopScorer);
-    
+
     void delete(LeagueTopScorer leagueTopScorer);
-    
+
     List<LeagueTopScorer> findByLeagueId(Long leagueId);
-    
+
     void deleteByLeagueId(Long leagueId);
-    
+
     @Query("SELECT new com.sports.server.command.team.domain.PlayerGoalCountWithRank(" +
-           "lts.player.id, lts.player.studentNumber, lts.player.name, SUM(lts.goalCount), " +
-           "ROW_NUMBER() OVER (ORDER BY SUM(lts.goalCount) DESC)) " +
-           "FROM LeagueTopScorer lts " +
-           "WHERE YEAR(lts.league.startAt) = :year " +
-           "GROUP BY lts.player " +
-           "ORDER BY SUM(lts.goalCount) DESC")
+            "lts.player.id, lts.player.studentNumber, lts.player.name, SUM(lts.goalCount), " +
+            "ROW_NUMBER() OVER (ORDER BY SUM(lts.goalCount) DESC)) " +
+            "FROM LeagueTopScorer lts " +
+            "WHERE YEAR(lts.league.startAt) = :year " +
+            "GROUP BY lts.player " +
+            "ORDER BY SUM(lts.goalCount) DESC")
     List<PlayerGoalCountWithRank> findTopPlayersByYearWithTotalGoals(@Param("year") Integer year, Pageable pageable);
+
+    List<LeagueTopScorer> saveAll(Iterable<LeagueTopScorer> leagueTopScorers);
 }
