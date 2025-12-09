81308f7112c9869660598200986f13c84d267459
Jin409 <sualng123@gmail.com>
2025-12-09 23:41:31 +0900

    [REFACTOR] 예외 처리 컨벤션에 맞게 수정

--- src/main/java/com/sports/server/command/cheertalk/application/mapper/CheerTalkFilterResponseMapperFinder.java
+++ src/main/java/com/sports/server/command/cheertalk/application/mapper/CheerTalkFilterResponseMapperFinder.java
@@ -1,6 +1,7 @@
 package com.sports.server.command.cheertalk.application.mapper;
 
 import com.sports.server.command.cheertalk.domain.BotType;
+import com.sports.server.common.exception.BadRequestException;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -23,7 +24,7 @@ public class CheerTalkFilterResponseMapperFinder {
     public CheerTalkFilterResponseMapper find(BotType botType) {
         CheerTalkFilterResponseMapper mapper = mapperMap.get(botType);
         if (mapper == null) {
-            throw new IllegalArgumentException("지원하지 않는 BotType 입니다: " + botType);
+            throw new BadRequestException("지원하지 않는 봇 타입입니다.");
         }
         return mapper;
     }
