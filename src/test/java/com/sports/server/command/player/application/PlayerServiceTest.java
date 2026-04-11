9307ce342311c33c2effa7cd6f18a6b69cb9d1d8
Jin409 <sualng123@gmail.com>
2026-04-12 00:41:37 +0900

    [TEST] 관련 테스트 추가

--- src/test/java/com/sports/server/command/player/application/PlayerServiceTest.java
+++ src/test/java/com/sports/server/command/player/application/PlayerServiceTest.java
@@ -6,6 +6,7 @@ import com.sports.server.command.player.domain.PlayerRepository;
 import com.sports.server.command.player.dto.PlayerRequest;
 import com.sports.server.common.application.EntityUtils;
 import com.sports.server.common.exception.CustomException;
+import com.sports.server.common.exception.ExceptionMessages;
 import com.sports.server.common.exception.NotFoundException;
 import com.sports.server.support.ServiceTest;
 import org.assertj.core.api.Assertions;
@@ -13,6 +14,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.jdbc.Sql;
 
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 @Sql("/member-fixture.sql")
@@ -25,6 +30,61 @@ public class PlayerServiceTest extends ServiceTest {
 
     @Autowired
     private EntityUtils entityUtils;
+
+    @Nested
+    @DisplayName("Organization별 학번 자릿수 검증")
+    class StudentNumberDigitsValidation {
+
+        @Test
+        void 학번_9자리_organization에서_9자리_학번으로_등록_성공() {
+            // given
+            Organization org = entityUtils.getEntity(1L, Organization.class); // student_number_digits = 9
+            PlayerRequest.Register request = new PlayerRequest.Register("손흥민", "202500001");
+
+            // when
+            Long playerId = playerService.register(request, org);
+
+            // then
+            assertThat(playerId).isNotNull();
+        }
+
+        @Test
+        void 학번_9자리_organization에서_10자리_학번으로_등록_시_예외가_발생한다() {
+            // given
+            Organization org = entityUtils.getEntity(1L, Organization.class); // student_number_digits = 9
+            PlayerRequest.Register request = new PlayerRequest.Register("손흥민", "2025000001");
+
+            // when & then
+            assertThatThrownBy(() -> playerService.register(request, org))
+                    .isInstanceOf(CustomException.class)
+                    .hasMessage(String.format(ExceptionMessages.PLAYER_STUDENT_NUMBER_INVALID, 9));
+        }
+
+        @Test
+        void 학번_10자리_organization에서_10자리_학번으로_등록_성공() {
+            // given
+            Organization org = entityUtils.getEntity(4L, Organization.class); // student_number_digits = 10
+            PlayerRequest.Register request = new PlayerRequest.Register("손흥민", "2025000001");
+
+            // when
+            Long playerId = playerService.register(request, org);
+
+            // then
+            assertThat(playerId).isNotNull();
+        }
+
+        @Test
+        void 학번_10자리_organization에서_9자리_학번으로_등록_시_예외가_발생한다() {
+            // given
+            Organization org = entityUtils.getEntity(4L, Organization.class); // student_number_digits = 10
+            PlayerRequest.Register request = new PlayerRequest.Register("손흥민", "202500001");
+
+            // when & then
+            assertThatThrownBy(() -> playerService.register(request, org))
+                    .isInstanceOf(CustomException.class)
+                    .hasMessage(String.format(ExceptionMessages.PLAYER_STUDENT_NUMBER_INVALID, 10));
+        }
+    }
 
     @Test
     void 선수_등록_시_학번이_중복되면_예외가_발생한다() {
